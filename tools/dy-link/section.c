1910719d0f973cbe38c920f4cc06e405363109e7
Antti Tiihala <Tiihala@users.noreply.github.com>
2018-11-02 22:02:50 +0200

    Refactor the linker
    The output should be exactly the same at this point. This commit was
    relatively big and might have introduced new bugs but now it is easy
    to design some kind of solution for grouped sections.

--- tools/dy-link/section.c
+++ tools/dy-link/section.c
@@ -53,6 +53,7 @@ static unsigned char *iterate_next(struct iterate *it)
 				return sec;
 			}
 		}
+		it->sec = 0u;
 	}
 	return NULL;
 }
@@ -60,43 +61,37 @@ static unsigned char *iterate_next(struct iterate *it)
 int section_data_size(struct options *opt, const char *name)
 {
 	int total_size = 0;
-	int i;
+	unsigned char *buf;
+	struct iterate it;
 
-	for (i = 0; i < opt->nr_mfiles; i++) {
-		unsigned char *buf = opt->mfiles[i].data;
-		unsigned long secs = LE16(&buf[2]);
+	iterate_init(&it, opt, name);
+	while ((buf = iterate_next(&it)) != NULL) {
+		unsigned long add = LE32(&buf[16]);
+		unsigned long bit = LE32(&buf[36]);
+		int align = (int)((bit & 0x00F00000ul) >> 20);
 
-		while (secs--) {
-			if (!strcmp((const char *)&buf[20], name)) {
-				unsigned long add = LE32(&buf[20 + 16]);
-				unsigned long bit = LE32(&buf[20 + 36]);
-				int align = (int)((bit & 0x00F00000ul) >> 20);
-
-				if (align)
-					align = 1 << (align - 1);
-				else
-					align = 16;
-				/*
-				 * Handle section alignments correctly. The
-				 * total size has the padding bytes included.
-				 */
-				if (total_size < INT_MAX - (align - 1)) {
-					unsigned t1 = (unsigned)total_size;
-					unsigned t2 = (unsigned)(align - 1);
-					total_size = (int)((t1 + t2) & ~t2);
-				} else {
-					return INT_MAX;
-				}
-				if (add < (unsigned long)INT_MAX) {
-					if (total_size < INT_MAX - (int)add)
-						total_size += (int)add;
-					else
-						return INT_MAX;
-				} else {
-					return INT_MAX;
-				}
-			}
-			buf += 40;
+		if (align)
+			align = 1 << (align - 1);
+		else
+			align = 16;
+		/*
+		 * Handle section alignments correctly. The
+		 * total size has the padding bytes included.
+		 */
+		if (total_size < INT_MAX - (align - 1)) {
+			unsigned t1 = (unsigned)total_size;
+			unsigned t2 = (unsigned)(align - 1);
+			total_size = (int)((t1 + t2) & ~t2);
+		} else {
+			return INT_MAX;
+		}
+		if (add < (unsigned long)INT_MAX) {
+			if (total_size < INT_MAX - (int)add)
+				total_size += (int)add;
+			else
+				return INT_MAX;
+		} else {
+			return INT_MAX;
 		}
 	}
 	return total_size;
@@ -105,27 +100,21 @@ int section_data_size(struct options *opt, const char *name)
 int section_reloc_size(struct options *opt, const char *name)
 {
 	int total_size = 0;
-	int i;
+	unsigned char *buf;
+	struct iterate it;
 
-	for (i = 0; i < opt->nr_mfiles; i++) {
-		unsigned char *buf = opt->mfiles[i].data;
-		unsigned long secs = LE16(&buf[2]);
+	iterate_init(&it, opt, name);
+	while ((buf = iterate_next(&it)) != NULL) {
+		unsigned long nr_rel = LE16(&buf[32]);
+		unsigned long add = nr_rel * 10ul;
 
-		while (secs--) {
-			if (!strcmp((const char *)&buf[20], name)) {
-				unsigned long nr_rel = LE16(&buf[20 + 32]);
-				unsigned long add = nr_rel * 10ul;
-
-				if (add < (unsigned long)INT_MAX) {
-					if (total_size < INT_MAX - (int)add)
-						total_size += (int)add;
-					else
-						return INT_MAX;
-				} else {
-					return INT_MAX;
-				}
-			}
-			buf += 40;
+		if (add < (unsigned long)INT_MAX) {
+			if (total_size < INT_MAX - (int)add)
+				total_size += (int)add;
+			else
+				return INT_MAX;
+		} else {
+			return INT_MAX;
 		}
 	}
 	return total_size;
@@ -164,50 +153,44 @@ int section_check_sizes(struct options *opt)
 int section_copy_d(struct options *opt, const char *name, unsigned char *out)
 {
 	int total_size = 0;
-	int i;
+	unsigned char *buf;
+	struct iterate it;
 
-	for (i = 0; i < opt->nr_mfiles; i++) {
-		unsigned char *dat = opt->mfiles[i].data;
-		unsigned char *buf = dat;
-		unsigned long secs = LE16(&buf[2]);
+	iterate_init(&it, opt, name);
+	while ((buf = iterate_next(&it)) != NULL) {
+		unsigned char *dat = opt->mfiles[it.obj].data;
+		unsigned long add = LE32(&buf[16]);
+		unsigned long off = LE32(&buf[20]);
+		unsigned long bit = LE32(&buf[36]);
+		int align = (int)((bit & 0x00F00000ul) >> 20);
 
-		while (secs--) {
-			if (!strcmp((const char *)&buf[20], name)) {
-				unsigned long add = LE32(&buf[20 + 16]);
-				unsigned long off = LE32(&buf[20 + 20]);
-				unsigned long bit = LE32(&buf[20 + 36]);
-				int align = (int)((bit & 0x00F00000ul) >> 20);
-
-				if (align)
-					align = 1 << (align - 1);
-				else
-					align = 16;
-				/*
-				 * Handle section alignments.
-				 */
-				{
-					unsigned t1 = (unsigned)total_size;
-					unsigned t2 = (unsigned)(align - 1);
-					total_size = (int)((t1 + t2) & ~t2);
-				}
-				/*
-				 * Set the offset so we can refer to the
-				 * correct data in the final object file.
-				 */
-				W_LE32(&buf[20 + 12], total_size);
-				/*
-				 * Copy the section data.
-				 */
-				if (out && add) {
-					void *t1 = (void *)(out + total_size);
-					void *t2 = (void *)(dat + off);
-					size_t t3 = (size_t)add;
-					memcpy(t1, (const void *)t2, t3);
-				}
-				total_size += (int)add;
-			}
-			buf += 40;
+		if (align)
+			align = 1 << (align - 1);
+		else
+			align = 16;
+		/*
+		 * Handle section alignments.
+		 */
+		{
+			unsigned t1 = (unsigned)total_size;
+			unsigned t2 = (unsigned)(align - 1);
+			total_size = (int)((t1 + t2) & ~t2);
 		}
+		/*
+		 * Set the offset so we can refer to the
+		 * correct data in the final object file.
+		 */
+		W_LE32(&buf[12], total_size);
+		/*
+		 * Copy the section data.
+		 */
+		if (out && add) {
+			void *t1 = (void *)(out + total_size);
+			void *t2 = (void *)(dat + off);
+			size_t t3 = (size_t)add;
+			memcpy(t1, (const void *)t2, t3);
+		}
+		total_size += (int)add;
 	}
 	return total_size;
 }
@@ -215,42 +198,43 @@ int section_copy_d(struct options *opt, const char *name, unsigned char *out)
 int section_copy_r(struct options *opt, const char *name, unsigned char *out)
 {
 	int total_size = 0;
-	int total_symbols = 0;
-	int i;
+	unsigned char *buf;
+	struct iterate it;
 
-	for (i = 0; i < opt->nr_mfiles; i++) {
-		unsigned char *dat = opt->mfiles[i].data;
-		unsigned char *buf = dat;
-		unsigned long secs = LE16(&buf[2]);
+	iterate_init(&it, opt, name);
+	while ((buf = iterate_next(&it)) != NULL) {
+		unsigned char *dat = opt->mfiles[it.obj].data;
+		unsigned long t_off = 0ul;
+		unsigned long s_off = LE32(&buf[12]);
+		unsigned long r_off = LE32(&buf[24]);
+		unsigned long r_num = LE16(&buf[32]);
+		unsigned char *src = dat + r_off;
+		unsigned char *dst = out + total_size;
+		unsigned objs = it.obj;
 
-		while (secs--) {
-			if (!strcmp((const char *)&buf[20], name)) {
-				unsigned long s_off = LE32(&buf[20 + 12]);
-				unsigned long r_off = LE32(&buf[20 + 24]);
-				unsigned long r_num = LE16(&buf[20 + 32]);
-				unsigned char *src = dat + r_off;
-				unsigned char *dst = out + total_size;
-
-				/*
-				 * Translate the section relocations.
-				 */
-				total_size += (int)r_num * 10;
-				while (r_num--) {
-					unsigned long r1 = LE32(&src[0]);
-					unsigned long r2 = LE32(&src[4]);
-					unsigned long r3 = LE16(&src[8]);
-
-					r1 += s_off;
-					r2 += total_symbols;
-					W_LE32(&dst[0], r1);
-					W_LE32(&dst[4], r2);
-					W_LE16(&dst[8], r3);
-					src += 10, dst += 10;
-				}
-			}
-			buf += 40;
+		/*
+		 * Figure out the global table offset.
+		 */
+		while (objs--) {
+			unsigned char *d = opt->mfiles[objs].data;
+			t_off += LE32(&d[12]);
 		}
-		total_symbols += (int)LE32(&dat[12]);
+		/*
+		 * Translate the section relocations.
+		 */
+		total_size += (int)r_num * 10;
+		while (r_num--) {
+			unsigned long r1 = LE32(&src[0]);
+			unsigned long r2 = LE32(&src[4]);
+			unsigned long r3 = LE16(&src[8]);
+
+			r1 += s_off;
+			r2 += t_off;
+			W_LE32(&dst[0], r1);
+			W_LE32(&dst[4], r2);
+			W_LE16(&dst[8], r3);
+			src += 10, dst += 10;
+		}
 	}
 	return total_size;
 }
