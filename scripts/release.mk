87d6164b6910bb7afd4285c06178dfb68c5995f8
Antti Tiihala <Tiihala@users.noreply.github.com>
2019-01-27 20:54:32 +0200

    Use correct timestamps
    There are two files that should use timestamps other than the current
    build time suggests. Small details like these are nice, sometimes.

--- scripts/release.mk
+++ scripts/release.mk
@@ -2,35 +2,40 @@
 
 ##############################################################################
 
+LDR512_FILE=-t 2018-01-23T21:49:13 --read-only
+LOADER_FILE=-t 2019-01-27T17:30:12 --read-only
+
+##############################################################################
+
 ./release/dancy.iso: $(DANCY_TARGET_SYSTEM)
 	$(DY_VBR) -t lba --2048 $@ 8192
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
 	$(DY_MCOPY) -i $@ ./system/IN_X64.AT ::system/IN_X64.AT
-	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512
-	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT
+	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
+	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT $(LOADER_FILE)
 	$(DY_ISO) -o $@ $@
 
 ./release/fdd160.img: $(DANCY_TARGET_SYSTEM)
 	$(DY_VBR) -t floppy $@ 160
 	$(DY_MCOPY) -i $@ ./system/IN_IA16.AT ::system/IN_IA16.AT
-	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512
-	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT
+	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
+	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT $(LOADER_FILE)
 
 ./release/fdd720.img: $(DANCY_TARGET_SYSTEM)
 	$(DY_VBR) -t floppy $@ 720
 	$(DY_MCOPY) -i $@ ./system/IN_IA16.AT ::system/IN_IA16.AT
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
 	$(DY_MCOPY) -i $@ ./system/IN_X64.AT ::system/IN_X64.AT
-	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512
-	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT
+	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
+	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT $(LOADER_FILE)
 
 ./release/fdd1440.img: $(DANCY_TARGET_SYSTEM)
 	$(DY_VBR) -t floppy $@ 1440
 	$(DY_MCOPY) -i $@ ./system/IN_IA16.AT ::system/IN_IA16.AT
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
 	$(DY_MCOPY) -i $@ ./system/IN_X64.AT ::system/IN_X64.AT
-	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512
-	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT
+	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
+	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT $(LOADER_FILE)
 
 ./release/dancy.zip: $(DANCY_TARGET_IMAGES) README VERSION
 	$(DY_ZIP) -o $@ $(DANCY_TARGET_IMAGES) ./README ./VERSION
