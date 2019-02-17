1752156a22302e9e818225b1cc6d4112d2cd5a2b
Antti Tiihala <Tiihala@users.noreply.github.com>
2019-02-17 20:51:11 +0200

    Clean up temporary code
    The init function still has some test code but I will try keep it very
    simple in the future. It could be a good starting point for checking
    and studying the flow of the initialization procedures. Next I'm going
    to write logging functions so that "memory maps" would not be printed
    on the screen during the normal system boot.
    
    A splash screen would be nice to have at some point. Just to make the
    operating system visually appealing if nothing else.

--- kernel/init/init.c
+++ kernel/init/init.c
@@ -19,121 +19,14 @@
 
 #include <dancy.h>
 
-static void print_memory_map(struct b_mem *mem)
-{
-	phys_addr_t total = 0;
-	size_t i;
-
-	b_print("Memory Map\n");
-
-	for (i = 0; i == 0 || mem[i].base; i++) {
-		uint32_t t = mem[i].type;
-		phys_addr_t b = mem[i].base;
-		phys_addr_t e = mem[i + 1].base - 1;
-		const char *desc = "Not Reported";
-
-		if (t == B_MEM_NORMAL)
-			desc = "Free", total += mem[i + 1].base - b;
-		else if (t == B_MEM_RESERVED)
-			desc = "Reserved";
-		else if (t == B_MEM_ACPI_RECLAIMABLE)
-			desc = "Acpi Reclaimable";
-		else if (t == B_MEM_ACPI_NVS)
-			desc = "Acpi NVS";
-		else if (t == B_MEM_BOOT_LOADER)
-			desc = "Loader Runtime (Reclaimable)";
-		else if (t == B_MEM_INIT_EXECUTABLE)
-			desc = "Executable (Init)";
-
-		if (t >= B_MEM_DATABASE_MIN && t <= B_MEM_DATABASE_MAX) {
-			unsigned db = (unsigned)t & 0x0000FFFFu;
-			b_print("  %p %p  Database (#%u)\n", b, e, db);
-		} else {
-			b_print("  %p %p  %s\n", b, e, desc);
-		}
-	}
-	b_print("\n  Total free: %zd KiB\n\n", total / 1024);
-}
-
-static void print_boot_information(void)
-{
-	struct b_video_edid edid;
-	struct b_video_info vi;
-	unsigned long val;
-
-	b_print("Boot Information\n");
-
-	val = b_get_parameter(B_A20_STATE);
-	if (val == B_A20_INT15H)
-		b_print("  A20 : enabled by int 0x15 method\n");
-	else if (val == B_A20_KEYBOARD)
-		b_print("  A20 : enabled by keyboard method\n");
-	else if (val == B_A20_FAST)
-		b_print("  A20 : enabled by \"fast\" method\n");
-	else if (val == B_A20_AUTOMATIC)
-		b_print("  A20 : was enabled automatically\n");
-
-	val = b_get_parameter(B_ACPI_POINTER);
-	if (val)
-		b_print("  Acpi: structure at %08lX (RSDP)\n", val);
-
-	if (b_get_structure(&vi, B_VIDEO_INFO))
-		b_print("  Vbe : framebuffer at %08lX\n", vi.framebuffer);
-
-	if (b_get_structure(&edid, B_VIDEO_EDID)) {
-		unsigned t1, t2;
-
-		t1 = (unsigned)edid.edid[0x12];
-		t2 = (unsigned)edid.edid[0x13];
-		b_print("  Edid: version %u (revision %u)\n", t1, t2);
-
-		t1 = (unsigned)edid.edid[0x15];
-		t2 = (unsigned)edid.edid[0x16];
-		b_print("  Edid: max hsize %u (cm)\n", t1);
-		b_print("  Edid: max vsize %u (cm)\n", t2);
-	}
-	b_print("\n");
-}
-
-static void sleep(int seconds)
-{
-	int i;
-
-	/*
-	 * This "sleep" function is not accurate. The pause
-	 * function waits approximately 500 milliseconds.
-	 */
-	for (i = 0; i < (seconds * 2); i++)
-		b_pause();
-}
-
 void init(void *map)
 {
-	print_memory_map(map);
-	print_boot_information();
+	const uint32_t required_mem = 0x00200000;
 
-	{
-		uint32_t eax, ecx, edx, ebx;
-		const char *fmt;
+	if (memory_init(map, required_mem))
+		return;
 
-		cpu_id((eax = 0, &eax), &ecx, &edx, &ebx);
-		fmt = "CPU Manufacturer\n  %.4s%.4s%.4s\n  ";
-		b_print(fmt, (char *)&ebx, (char *)&edx, (char *)&ecx);
-
-		cpu_id((eax = 1, &eax), &ecx, &edx, &ebx);
-		if (edx & (1 << 0))
-			b_print("FPU ");
-		if (edx & (1 << 1))
-			b_print("VME ");
-		if (edx & (1 << 2))
-			b_print("DE ");
-		if (edx & (1 << 3))
-			b_print("PSE ");
-		if (edx & (1 << 4))
-			b_print("TSC ");
-
-		b_print("...\n\n");
-	}
+	memory_print_map(0);
 
 	for (;;) {
 		uint32_t low, high;
@@ -145,7 +38,7 @@ void init(void *map)
 		b_print("cpu_rdtsc_diff: %08X%08X\r", high, low);
 
 		cpu_rdtsc(&low, &high);
-		sleep(1);
+		b_pause();
 		cpu_rdtsc_diff(&low, &high);
 		cpu_rdtsc_delay(low, high);
 	}
