9e159b248e04692ba83bf6ef4966fc140cd14ffa
Antti Tiihala <Tiihala@users.noreply.github.com>
2019-02-23 20:51:08 +0200

    Improve blob.h
    These preprocessor macros have worked well, e.g. dy-link has used them
    quite a lot. However, using these for aligned data adds some overhead.

--- include/dancy/blob.h
+++ include/dancy/blob.h
@@ -30,4 +30,18 @@
 (unsigned)(g) >> 8 & 0xFFu, (unsigned)(g) & 0xFFu, \
 (unsigned)(h) >> 8 & 0xFFu, (unsigned)(h) & 0xFFu
 
+#define B8(a,b,c) (((unsigned long)((a)[(b)]) & 0xFFul) << (c))
+#define LE16(a) (B8((a),0,0) | B8((a),1,8))
+#define LE32(a) (B8((a),0,0) | B8((a),1,8) | B8((a),2,16) | B8((a),3,24))
+
+#define W_LE16(a,d) ( \
+*((a) + 0) = (unsigned char)(((unsigned)(d) >> 0) & 0xFFu), \
+*((a) + 1) = (unsigned char)(((unsigned)(d) >> 8) & 0xFFu))
+
+#define W_LE32(a,d) ( \
+*((a) + 0) = (unsigned char)(((unsigned long)(d) >>  0) & 0xFFul), \
+*((a) + 1) = (unsigned char)(((unsigned long)(d) >>  8) & 0xFFul), \
+*((a) + 2) = (unsigned char)(((unsigned long)(d) >> 16) & 0xFFul), \
+*((a) + 3) = (unsigned char)(((unsigned long)(d) >> 24) & 0xFFul))
+
 #endif
