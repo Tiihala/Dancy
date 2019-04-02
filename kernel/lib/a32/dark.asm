8fda46994b76c5889b70e0a75aed87423aa4bfa5
Antti Tiihala <Tiihala@users.noreply.github.com>
2019-04-02 22:01:25 +0300

    Add helper routines
    It is extremely important that these functions are bug-free. Otherwise
    the whole operating system is erroneous. However, there should not be
    many 64-bit div or mod operations needed on 32-bit CPUs. These helper
    routines are highly inefficient so the most important optimization is
    to avoid calling them in the first place. The reason I added these was
    to make the "ACPI_USE_NATIVE_DIVIDE" work.
    
    If the performance of these helper routines becomes a problem in the
    future, I have to think about other optimizations.

--- kernel/lib/a32/dark.asm
+++ kernel/lib/a32/dark.asm
@@ -21,8 +21,118 @@
 
 section .text
 
+        global __aulldiv
+        global __aullrem
         global ___chkstk_ms
         global __chkstk
+        global ___udivdi3
+        global ___umoddi3
+
+align 16
+__aulldiv:
+        push ecx                        ; save register ecx
+        push ebx                        ; save register ebx
+        xor eax, eax                    ; eax = 0 (remainder, low dword)
+        xor edx, edx                    ; edx = 0 (remainder, high dword)
+        push eax                        ; push "quotient" (high dword)
+        push edx                        ; push "quotient" (low dword)
+
+        mov ecx, 63                     ; ecx = loop counter
+        mov ebx, [esp+28]               ; test variable "b" (low dword)
+        or ebx, [esp+32]                ; test variable "b" (high dword)
+        jnz short .L1
+        div ebx                         ; trigger "div by zero" exception
+
+.L1:    shl eax, 1                      ; remainder <<= 1
+        rcl edx, 1                      ; (high dword)
+        mov ebx, 1                      ; ebx = 1
+        rol ebx, cl                     ; rotate
+        cmp ecx, 32                     ; low or high dword
+        jb short .L2
+        and ebx, [esp+24]               ; variable "a" (high dword)
+        jmp short .L3
+.L2:    and ebx, [esp+20]               ; variable "a" (low dword)
+.L3:    jz short .L4
+        or eax, 1                       ; set least significant bit
+
+.L4:    push eax                        ; save register eax
+        push edx                        ; save register edx
+        sub eax, [esp+36]               ; variable "b" (low dword)
+        sbb edx, [esp+40]               ; variable "b" (high dword)
+        jnc short .L5
+        pop edx                         ; restore register edx
+        pop eax                         ; restore register eax
+        jmp short .L7
+
+.L5:    add esp, 8                      ; remove eax and edx from stack
+        mov ebx, 1                      ; ebx = 1
+        rol ebx, cl                     ; rotate
+        cmp ecx, 32                     ; low or high dword
+        jb short .L6
+        or [esp+4], ebx                 ; set bit (quotient, high dword)
+        jmp short .L7
+.L6:    or [esp], ebx                   ; set bit (quotient, low dword)
+.L7:    sub ecx, 1                      ; decrement counter
+        jnc short .L1
+
+        mov eax, [esp]                  ; eax = quotient (low dword)
+        mov edx, [esp+4]                ; eax = quotient (high dword)
+        add esp, 8                      ; remove stack variables
+        pop ebx                         ; restore register ebx
+        pop ecx                         ; restore register ecx
+        ret 16
+
+align 16
+__aullrem:
+        push ecx                        ; save register ecx
+        push ebx                        ; save register ebx
+        xor eax, eax                    ; eax = 0 (remainder, low dword)
+        xor edx, edx                    ; edx = 0 (remainder, high dword)
+        push eax                        ; push "quotient" (high dword)
+        push edx                        ; push "quotient" (low dword)
+
+        mov ecx, 63                     ; ecx = loop counter
+        mov ebx, [esp+28]               ; test variable "b" (low dword)
+        or ebx, [esp+32]                ; test variable "b" (high dword)
+        jnz short .L1
+        div ebx                         ; trigger "div by zero" exception
+
+.L1:    shl eax, 1                      ; remainder <<= 1
+        rcl edx, 1                      ; (high dword)
+        mov ebx, 1                      ; ebx = 1
+        rol ebx, cl                     ; rotate
+        cmp ecx, 32                     ; low or high dword
+        jb short .L2
+        and ebx, [esp+24]               ; variable "a" (high dword)
+        jmp short .L3
+.L2:    and ebx, [esp+20]               ; variable "a" (low dword)
+.L3:    jz short .L4
+        or eax, 1                       ; set least significant bit
+
+.L4:    push eax                        ; save register eax
+        push edx                        ; save register edx
+        sub eax, [esp+36]               ; variable "b" (low dword)
+        sbb edx, [esp+40]               ; variable "b" (high dword)
+        jnc short .L5
+        pop edx                         ; restore register edx
+        pop eax                         ; restore register eax
+        jmp short .L7
+
+.L5:    add esp, 8                      ; remove eax and edx from stack
+        mov ebx, 1                      ; ebx = 1
+        rol ebx, cl                     ; rotate
+        cmp ecx, 32                     ; low or high dword
+        jb short .L6
+        or [esp+4], ebx                 ; set bit (quotient, high dword)
+        jmp short .L7
+.L6:    or [esp], ebx                   ; set bit (quotient, low dword)
+.L7:    sub ecx, 1                      ; decrement counter
+        jnc short .L1
+
+        add esp, 8                      ; remove stack variables
+        pop ebx                         ; restore register ebx
+        pop ecx                         ; restore register ecx
+        ret 16
 
 align 16
 ___chkstk_ms:
@@ -36,4 +146,22 @@ __chkstk:
         mov ecx, [esp+eax+4]            ; ecx = instruction pointer
         mov [esp+4], ecx                ; set return address
         pop ecx                         ; restore register ecx
+        ret
+
+align 16
+___udivdi3:
+        push dword [esp+16]             ; push "b" (high dword)
+        push dword [esp+16]             ; push "b" (low dword)
+        push dword [esp+16]             ; push "a" (high dword)
+        push dword [esp+16]             ; push "a" (low dword)
+        call __aulldiv                  ; (stack cleaned by callee)
+        ret
+
+align 16
+___umoddi3:
+        push dword [esp+16]             ; push "b" (high dword)
+        push dword [esp+16]             ; push "b" (low dword)
+        push dword [esp+16]             ; push "a" (high dword)
+        push dword [esp+16]             ; push "a" (low dword)
+        call __aullrem                  ; (stack cleaned by callee)
         ret
