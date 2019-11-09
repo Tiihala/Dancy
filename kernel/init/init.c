7581cdc8eb8b15eeac45405f127f2c9fbff80476
Antti Tiihala <Tiihala@users.noreply.github.com>
2019-11-09 18:47:23 +0200

    Add keyboard test code
    This just prints the key names, e.g. DANCY_KEY_A, DANCY_KEY_B, etc.

--- kernel/init/init.c
+++ kernel/init/init.c
@@ -53,6 +53,108 @@ void init(void *map)
 	 * Temporary code for testing purposes.
 	 */
 	{
+		static const char *key_name[] = {
+			"DANCY_KEY_NULL",
+			"DANCY_KEY_ERROR1",
+			"DANCY_KEY_ERROR2",
+			"DANCY_KEY_UNDEFINED",
+			"DANCY_KEY_A",
+			"DANCY_KEY_B",
+			"DANCY_KEY_C",
+			"DANCY_KEY_D",
+			"DANCY_KEY_E",
+			"DANCY_KEY_F",
+			"DANCY_KEY_G",
+			"DANCY_KEY_H",
+			"DANCY_KEY_I",
+			"DANCY_KEY_J",
+			"DANCY_KEY_K",
+			"DANCY_KEY_L",
+			"DANCY_KEY_M",
+			"DANCY_KEY_N",
+			"DANCY_KEY_O",
+			"DANCY_KEY_P",
+			"DANCY_KEY_Q",
+			"DANCY_KEY_R",
+			"DANCY_KEY_S",
+			"DANCY_KEY_T",
+			"DANCY_KEY_U",
+			"DANCY_KEY_V",
+			"DANCY_KEY_W",
+			"DANCY_KEY_X",
+			"DANCY_KEY_Y",
+			"DANCY_KEY_Z",
+			"DANCY_KEY_1",
+			"DANCY_KEY_2",
+			"DANCY_KEY_3",
+			"DANCY_KEY_4",
+			"DANCY_KEY_5",
+			"DANCY_KEY_6",
+			"DANCY_KEY_7",
+			"DANCY_KEY_8",
+			"DANCY_KEY_9",
+			"DANCY_KEY_0",
+			"DANCY_KEY_ENTER",
+			"DANCY_KEY_ESCAPE",
+			"DANCY_KEY_BACKSPACE",
+			"DANCY_KEY_TAB",
+			"DANCY_KEY_SPACE",
+			"DANCY_KEY_UNDERSCORE",
+			"DANCY_KEY_EQUALS",
+			"DANCY_KEY_LEFTBRACKET",
+			"DANCY_KEY_RIGHTBRACKET",
+			"DANCY_KEY_BACKSLASH",
+			"DANCY_KEY_HASH",
+			"DANCY_KEY_SEMICOLON",
+			"DANCY_KEY_APOSTROPHE",
+			"DANCY_KEY_GRAVE",
+			"DANCY_KEY_COMMA",
+			"DANCY_KEY_PERIOD",
+			"DANCY_KEY_SLASH",
+			"DANCY_KEY_CAPSLOCK",
+			"DANCY_KEY_F1",
+			"DANCY_KEY_F2",
+			"DANCY_KEY_F3",
+			"DANCY_KEY_F4",
+			"DANCY_KEY_F5",
+			"DANCY_KEY_F6",
+			"DANCY_KEY_F7",
+			"DANCY_KEY_F8",
+			"DANCY_KEY_F9",
+			"DANCY_KEY_F10",
+			"DANCY_KEY_F11",
+			"DANCY_KEY_F12",
+			"DANCY_KEY_PRINTSCREEN",
+			"DANCY_KEY_SCROLLLOCK",
+			"DANCY_KEY_PAUSE",
+			"DANCY_KEY_INSERT",
+			"DANCY_KEY_HOME",
+			"DANCY_KEY_PAGEUP",
+			"DANCY_KEY_DELETE",
+			"DANCY_KEY_END",
+			"DANCY_KEY_PAGEDOWN",
+			"DANCY_KEY_RIGHTARROW",
+			"DANCY_KEY_LEFTARROW",
+			"DANCY_KEY_DOWNARROW",
+			"DANCY_KEY_UPARROW",
+			"DANCY_KEY_NUMLOCK",
+			"DANCY_KEY_PADSLASH",
+			"DANCY_KEY_PADASTERISK",
+			"DANCY_KEY_PADMINUS",
+			"DANCY_KEY_PADPLUS",
+			"DANCY_KEY_PADENTER",
+			"DANCY_KEY_PAD1",
+			"DANCY_KEY_PAD2",
+			"DANCY_KEY_PAD3",
+			"DANCY_KEY_PAD4",
+			"DANCY_KEY_PAD5",
+			"DANCY_KEY_PAD6",
+			"DANCY_KEY_PAD7",
+			"DANCY_KEY_PAD8",
+			"DANCY_KEY_PAD9",
+			"DANCY_KEY_PAD0",
+			"DANCY_KEY_PADDELETE"
+		};
 		size_t i;
 
 		for (i = 0; i < boot_log_size + 1; i++) {
@@ -65,6 +167,15 @@ void init(void *map)
 		}
 
 		b_output_string(boot_log, (unsigned)boot_log_size);
+
+		for (;;) {
+			unsigned long r = b_get_keycode();
+
+			if (r == 0)
+				continue;
+			b_print("%04lX ", r >> 16);
+			b_print("%s\n", key_name[r & 0xFFul]);
+		}
 	}
 
 	while (!b_pause()) { }
