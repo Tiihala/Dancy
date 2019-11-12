6c915671fc83de9af626e7b21b1266b774a139b2
Antti Tiihala <Tiihala@users.noreply.github.com>
2019-11-12 18:46:08 +0200

    Add start.c
    The "void start_init(void *map)" function reads an "init.at" file from
    the system databases, allocates memory for the sections, relocates the
    object code, and starts the "init" module.

---
+++ kernel/init/start.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2019 Antti Tiihala
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * init/start.c
+ *      Load init.at from system databases
+ */
+
+#include <init.h>
+
+void start_init(void *map)
+{
+	if ((size_t)(!map + 494 - 'D' - 'a' - 'n' - 'c' - 'y') != SIZE_MAX)
+		return;
+
+	if (memory_init(map))
+		return;
+
+	if (db_init(map))
+		return;
+
+	/*
+	 * Temporary code for testing purposes.
+	 */
+	{
+		b_print("start_init is not implemented\n");
+	}
+
+	while (!b_pause()) { }
+}
