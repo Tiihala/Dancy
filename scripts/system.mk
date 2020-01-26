26c7b613c28f9b87e20cb02b0576d9e6084ab555
Antti Tiihala <Tiihala@users.noreply.github.com>
2020-01-26 21:07:51 +0200

    Export IN_IA32.AT and IN_X64.AT symbols
    The dynamically linked o32/init.at or o64/init.at use these exported
    symbols (e.g. memcpy, memset).

--- scripts/system.mk
+++ scripts/system.mk
@@ -17,11 +17,11 @@ DANCY_DB_FILES= \
 	$(DY_ZIP) -o$@ $(DANCY_DB_FILES)
 
 ./system/IN_IA32.AT: $(DANCY_START_OBJECTS_32)
-	$(DY_LINK) -o$@ -finit $(DANCY_START_OBJECTS_32)
+	$(DY_LINK) -o$@ -finit --export-all $(DANCY_START_OBJECTS_32)
 	$(DY_INIT) -tia32 --set-header $@
 
 ./system/IN_X64.AT: $(DANCY_START_OBJECTS_64)
-	$(DY_LINK) -o$@ -finit $(DANCY_START_OBJECTS_64)
+	$(DY_LINK) -o$@ -finit --export-all $(DANCY_START_OBJECTS_64)
 	$(DY_INIT) -tx64 --set-header $@
 
 ./LOADER.512: ./bin/dy-blob$(DANCY_EXE)
