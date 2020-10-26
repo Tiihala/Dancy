8e1b9c2488d3898c68786c9b56adea490f0d37bb
Antti Tiihala <Tiihala@users.noreply.github.com>
2020-10-26 20:48:48 +0200

    Add idt_panic function
    If there is an unhandled exception or interrupt, print a system panic
    message. The pre-kernel environment doesn't try to recover the system.
    The first 32 vectors are reserved ("exceptions") and vectors from 32
    to 255 are "interrupts". However, the NMI is more like an interrupt.
    
    The AMD64 Architecture Programmer's Manual (Volume 2) says:
    
       "Exceptions are considered synchronous events because they are
        a direct result of executing the interrupted instruction."
    
       "External interrupts are asynchronous events that occur
        independently of the interrupted instruction."
    
    I try to be careful and use the correct terminology. The safest option
    could be to use "vectors" from 0 to 255.

--- kernel/init/idt.c
+++ kernel/init/idt.c
@@ -19,15 +19,66 @@
 
 #include <init.h>
 
+static void idt_panic(unsigned num, unsigned err_code, const void *stack)
+{
+	static const char *names[] = {
+		"#DE - Divide-by-Zero Exception",
+		"#DB - Debug Exception",
+		"NMI - Non-Maskable-Interrupt Exception",
+		"#BP - Breakpoint Exception",
+
+		"#OF - Overflow Exception",
+		"#BR - Bound-Range Exception",
+		"#UD - Invalid-Opcode Exception",
+		"#NM - Device-Not-Available Exception",
+
+		"#DF - Double-Fault Exception",
+		"Coprocessor-Segment-Overrun Exception",
+		"#TS - Invalid-TSS Exception",
+		"#NP - Segment-Not-Present Exception",
+
+		"#SS - Stack Exception",
+		"#GP - General-Protection Exception",
+		"#PF - Page-Fault Exception"
+	};
+	unsigned entries = (unsigned)(sizeof(names) / sizeof(names[0]));
+	const size_t size = 128;
+	char message[1024], *ptr;
+	int add, i;
+
+	ptr = &message[0];
+	add = snprintf(ptr, size, "   **** SYSTEM PANIC ****\n\n");
+	ptr += ((add > 0) ? add : 0);
+
+	if (num < entries)
+		add = snprintf(ptr, size, "%s\n\n", names[num]);
+	else
+		add = snprintf(ptr, size, "Interrupt Vector %u\n\n", num);
+	ptr += ((add > 0) ? add : 0);
+
+	if (num == 14 && err_code != 0) {
+		phys_addr_t cr2;
+
+		pg_get_fault(&cr2);
+		add = snprintf(ptr, size, "CR2: %p\n\n", cr2);
+		ptr += ((add > 0) ? add : 0);
+	}
+
+	for (i = 1; i >= -1; i--) {
+		const phys_addr_t *s = (const phys_addr_t *)stack;
+		const void *val = (const void *)s[i];
+
+		add = snprintf(ptr, size, "  [%p]  %p\n", (s + i), val);
+		ptr += ((add > 0) ? add : 0);
+	}
+
+	snprintf(ptr, size, "\nPlease restart the computer. Halted.");
+	gui_print_alert(&message[0]);
+}
+
 void idt_handler(unsigned num, unsigned err_code, const void *stack)
 {
 	const unsigned pic_irq_base = 32;
-
-	/*
-	 * Ignore err_code and stack.
-	 */
-	(void)err_code;
-	(void)stack;
 
 	/*
 	 * Nested exceptions are not allowed. If an exception
@@ -52,6 +103,7 @@ void idt_handler(unsigned num, unsigned err_code, const void *stack)
 			return;
 		}
 
+		idt_panic(num, err_code, stack);
 		cpu_halt(0);
 	}
 
@@ -69,5 +121,6 @@ void idt_handler(unsigned num, unsigned err_code, const void *stack)
 		return;
 	}
 
+	idt_panic(num, err_code, stack);
 	cpu_halt(0);
 }
