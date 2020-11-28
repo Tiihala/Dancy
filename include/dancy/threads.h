a4d4ecc4a300c23df9f928c7a9c15e1b7bb61477
Antti Tiihala <Tiihala@users.noreply.github.com>
2020-11-28 22:04:08 +0200

    Add threads.h
    This header was added in C11 and includes multi-threading support. It
    seems that this is not very widely used because many operating systems
    have their own established ways to handle multi-threading.

---
+++ include/dancy/threads.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Antti Tiihala
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * dancy/threads.h
+ *      Header of Dancy Operating System
+ */
+
+#ifndef DANCY_THREADS_H
+#define DANCY_THREADS_H
+
+#include <dancy/time.h>
+
+typedef void *thrd_t;
+
+typedef int (*thrd_start_t)(void *);
+
+enum dancy_thrd_constants {
+	thrd_success,
+	thrd_busy,
+	thrd_error,
+	thrd_nomem,
+	thrd_timedout
+};
+
+int thrd_create(thrd_t *thr, thrd_start_t func, void *arg);
+thrd_t thrd_current(void);
+int thrd_detach(thrd_t thr);
+int thrd_equal(thrd_t thr0, thrd_t thr1);
+void thrd_exit(int res);
+int thrd_join(thrd_t thr, int *res);
+int thrd_sleep(const struct timespec *duration, struct timespec *remaining);
+void thrd_yield(void);
+
+#endif
