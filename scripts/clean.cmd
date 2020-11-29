9b76d21ec7347ec9a35a93bc72041a71eb3eb521
Antti Tiihala <Tiihala@users.noreply.github.com>
2020-11-29 11:13:06 +0200

    Update clean.cmd

--- scripts/clean.cmd
+++ scripts/clean.cmd
@@ -12,7 +12,6 @@ RMDIR /S /Q kernel\acpica > NUL 2>&1
 RMDIR /S /Q o32 > NUL 2>&1
 RMDIR /S /Q o64 > NUL 2>&1
 RMDIR /S /Q release > NUL 2>&1
-RMDIR /S /Q system > NUL 2>&1
 
 DEL scripts\dancy.mk > NUL 2>&1
 DEL LOADER.512 > NUL 2>&1
