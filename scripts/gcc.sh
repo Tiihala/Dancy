d9f9ae650119ba058292d0c1a021f4387121403e
Antti Tiihala <Tiihala@users.noreply.github.com>
2020-12-12 14:10:31 +0200

    Modify gcc.sh
    It is possible to use the compiler that is installed on the system.
    The structure of the script is like in the "asm.sh" file.

--- scripts/gcc.sh
+++ scripts/gcc.sh
@@ -7,10 +7,23 @@ then
     exit 1
 fi
 
+if [ -f "./external/bin/x86_64-w64-mingw32-gcc" ]
+then
+    echo "./external/bin/x86_64-w64-mingw32-gcc"
+    exit 0
+fi
+
 if [ -f "/usr/bin/x86_64-pc-msys-gcc.exe" ]
 then
     echo "x86_64-pc-msys-gcc"
     exit 0
 fi
 
-echo "./external/bin/x86_64-w64-mingw32-gcc"
+if [ -f "/usr/bin/x86_64-w64-mingw32-gcc" ]
+then
+    echo "/usr/bin/x86_64-w64-mingw32-gcc"
+    exit 0
+fi
+
+echo "Error: compiler is not found" 1>&2
+exit 1
