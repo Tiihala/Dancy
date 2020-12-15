fcc90bf7a32af9391b51b5d98978c1a7d5d8f109
Antti Tiihala <Tiihala@users.noreply.github.com>
2020-12-15 17:28:14 +0200

    Move dancy.ttf
    The file has been moved into share/fonts directory.

--- scripts/system.mk
+++ scripts/system.mk
@@ -3,7 +3,7 @@
 ##############################################################################
 
 DANCY_DB_FILES= \
- ./system/fonts/dancy.ttf \
+ ./share/fonts/dancy.ttf \
  $(ACPICA_O32_AT) \
  ./o32/init.at \
  $(ACPICA_O64_AT) \
