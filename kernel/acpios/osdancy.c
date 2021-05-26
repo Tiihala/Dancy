f7f8dc2b185fb563c2140c38967893cfc4b35cb6
Antti Tiihala <Tiihala@users.noreply.github.com>
2021-05-26 22:26:10 +0300

    Implement override functions
    My operating system does not override objects or tables at this point.

--- kernel/acpios/osdancy.c
+++ kernel/acpios/osdancy.c
@@ -253,3 +253,38 @@ void AcpiOsUnmapMemory(void *LogicalAddress, ACPI_SIZE Size)
 	(void)LogicalAddress;
 	(void)Size;
 }
+
+ACPI_STATUS AcpiOsPredefinedOverride(
+	const ACPI_PREDEFINED_NAMES *InitVal, ACPI_STRING *NewVal)
+{
+	if (!InitVal || !NewVal)
+		return (AE_BAD_PARAMETER);
+
+	*NewVal = NULL;
+
+	return (AE_OK);
+}
+
+ACPI_STATUS AcpiOsTableOverride(
+	ACPI_TABLE_HEADER *ExistingTable, ACPI_TABLE_HEADER **NewTable)
+{
+	if (!ExistingTable || !NewTable)
+		return (AE_BAD_PARAMETER);
+
+	*NewTable = NULL;
+
+	return (AE_OK);
+}
+
+ACPI_STATUS AcpiOsPhysicalTableOverride(
+	ACPI_TABLE_HEADER *ExistingTable, ACPI_PHYSICAL_ADDRESS *NewAddress,
+	UINT32 *NewTableLength)
+{
+	if (!ExistingTable || !NewAddress || !NewTableLength)
+		return (AE_BAD_PARAMETER);
+
+	*NewAddress = 0;
+	*NewTableLength = 0;
+
+	return (AE_OK);
+}
