70a2044a6404ae31f1c717abfd0f125017dbac55
Antti Tiihala <Tiihala@users.noreply.github.com>
2021-10-21 21:52:14 +0300

    Renew delay_calibrate function
    I have had a lot of problems with Time Stamp Counters (TSC) on virtual
    computers. If I remember correctly, and assuming there were no bugs in
    my own code, the RDTSC instruction sometimes returned a value that was
    less than the value read previously. On real hardware, and on the same
    specific processor, that should not happen.
    
    This new version of the delay_calibrate function has been simplified.
    It will wait 100 milliseconds and then calculates the difference.

--- boot/init/delay.c
+++ boot/init/delay.c
@@ -23,6 +23,9 @@ volatile int delay_ready = 0;
 volatile int delay_error = 0;
 volatile uint64_t delay_tsc_hz = 0;
 
+static uint32_t delay_call_count = 0;
+static uint64_t delay_prev_value = 0;
+
 /*
  * void delay_calibrate(void)
  *
@@ -30,6 +33,63 @@ volatile uint64_t delay_tsc_hz = 0;
  */
 void delay_calibrate(void)
 {
+	const uint32_t base_count = 32;
+	uint32_t tsc_a, tsc_d;
+	uint64_t tsc_diff, tsc_val;
+
+	if (delay_error != 0 || delay_ready != 0)
+		return;
+
+	if (cpu_add32(&delay_call_count, 1) < base_count)
+		return;
+
+	/*
+	 * Read the reference value first.
+	 */
+	if (delay_call_count == base_count) {
+		cpu_rdtsc(&tsc_a, &tsc_d);
+		tsc_val = (((uint64_t)tsc_d << 16) << 16) | (uint64_t)tsc_a;
+
+		delay_prev_value = tsc_val;
+		return;
+	}
+
+	if (delay_call_count < base_count + 100)
+		return;
+
+	/*
+	 * Read the value about 100 milliseconds later.
+	 */
+	cpu_rdtsc(&tsc_a, &tsc_d);
+	tsc_val = (((uint64_t)tsc_d << 16) << 16) | (uint64_t)tsc_a;
+
+	/*
+	 * The new value from the Time-Stamp Counter must
+	 * be larger than the previously read value.
+	 */
+	if (delay_prev_value >= tsc_val) {
+		delay_error = 2;
+		return;
+	}
+
+	tsc_diff = tsc_val - delay_prev_value;
+	delay_tsc_hz = tsc_diff * 10;
+
+	/*
+	 * The CPU specifications say that Time-Stamp Counters will
+	 * not wraparound very soon. For example, Intel:
+	 *
+	 * "...will not wraparound within 10 years after being reset."
+	 */
+	if ((delay_tsc_hz / 1000) > 0xFFFFFFFF) {
+		delay_error = 3;
+		return;
+	}
+
+	/*
+	 * delay_ready = 1;
+	 */
+	spin_lock((int *)&delay_ready);
 }
 
 void delay(uint32_t nanoseconds)
