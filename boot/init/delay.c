eaf1029913e4a320eaceef34feaf4d5048894b4e
Antti Tiihala <Tiihala@users.noreply.github.com>
2021-10-21 21:25:47 +0300

    Clear delay_calibrate function
    I will rewrite the whole function and keep the diff log readable.

--- boot/init/delay.c
+++ boot/init/delay.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Antti Tiihala
+ * Copyright (c) 2020, 2021 Antti Tiihala
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -23,27 +23,6 @@ volatile int delay_ready = 0;
 volatile int delay_error = 0;
 volatile uint64_t delay_tsc_hz = 0;
 
-#define DELAY_COUNTER 10
-#define DELAY_RETRIES 400
-
-static int delay_counter = DELAY_COUNTER;
-static int delay_retries = DELAY_RETRIES;
-
-static uint64_t delay_min_value = ULLONG_MAX;
-static uint64_t delay_max_value = 0;
-static uint64_t delay_prev_value = 0;
-
-static void delay_clear(int end)
-{
-	delay_counter = DELAY_COUNTER;
-	if (end)
-		delay_retries = DELAY_RETRIES;
-
-	delay_min_value = ULLONG_MAX;
-	delay_max_value = 0;
-	delay_prev_value = 0;
-}
-
 /*
  * void delay_calibrate(void)
  *
@@ -51,91 +30,6 @@ static void delay_clear(int end)
  */
 void delay_calibrate(void)
 {
-	uint32_t tsc_a, tsc_d;
-	uint64_t tsc_diff, tsc_val;
-
-	if (delay_error != 0)
-		return;
-
-	if (delay_retries <= 0) {
-		delay_error = 1;
-		return;
-	}
-
-	cpu_rdtsc(&tsc_a, &tsc_d);
-	tsc_val = (((uint64_t)tsc_d << 16) << 16) | (uint64_t)tsc_a;
-
-	/*
-	 * Read the reference value first.
-	 */
-	if (delay_prev_value == 0) {
-		delay_prev_value = tsc_val;
-		return;
-	}
-
-	/*
-	 * The new value from the Time-Stamp Counter must
-	 * be larger than the previously read value.
-	 */
-	if (delay_prev_value >= tsc_val) {
-		delay_error = 2;
-		return;
-	}
-
-	tsc_diff = tsc_val - delay_prev_value;
-	delay_prev_value = tsc_val;
-
-	/*
-	 * Check the minimum value and continue sampling
-	 * if performance is getting better.
-	 */
-	if (delay_min_value > tsc_diff) {
-		delay_min_value = tsc_diff;
-		delay_counter += 1;
-	}
-
-	if (delay_max_value < tsc_diff)
-		delay_max_value = tsc_diff;
-
-	if (delay_counter > 0) {
-		delay_counter -= 1;
-		return;
-	}
-
-	/*
-	 * Check that sample values are stable enough.
-	 */
-	{
-		uint64_t value_diff = delay_max_value - delay_min_value;
-		int value_rsh = (delay_retries < (DELAY_RETRIES / 2)) ? 3 : 8;
-
-		if (value_diff > (delay_min_value >> value_rsh)) {
-			delay_clear(0);
-			delay_retries -= 1;
-			return;
-		}
-
-		/*
-		 * Check that the max value can be an uint32_t. This is mostly
-		 * theoretical because CPU specifications say that Time-Stamp
-		 * Counters will not wraparound very soon. For example, Intel:
-		 *
-		 * "...will not wraparound within 10 years after being reset."
-		 */
-		if (delay_max_value > 0xFFFFFFFF) {
-			delay_error = 3;
-			return;
-		}
-
-		delay_tsc_hz = delay_max_value * 1000;
-	}
-
-	delay_clear(1);
-
-	/*
-	 * delay_ready = 1;
-	 */
-	spin_lock((int *)&delay_ready);
 }
 
 void delay(uint32_t nanoseconds)
