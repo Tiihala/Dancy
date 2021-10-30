f4448ae507a887e6a3f7e0ddbabe15b2705a0ace
Antti Tiihala <Tiihala@users.noreply.github.com>
2021-10-30 18:14:58 +0300

    Extend the stack pointer
    The task structure makes it possible to save the full 64-bit value. I
    want to have a separate commit for this modification because I need to
    carefully check its correctness. An unfortunate drawback is that now
    instructions take up slightly more space.

--- kernel/base/a64/task.asm
+++ kernel/base/a64/task.asm
@@ -45,15 +45,15 @@ align 16
         ;         ...
         ; };
 task_create_asm:
-        lea eax, [rcx+0x1FD0]           ; rax = stack pointer
+        lea rax, [rcx+0x1FD0]           ; rax = stack pointer
 
         mov [rax+8], rdx                ; save func
         mov [rax+16], r8                ; save arg
         mov edx, func_start             ; rdx = func_start
         mov [rax], rdx                  ; save func_start
 
-        lea eax, [rax-96]               ; space for pushed registers
-        mov [rcx], eax                  ; new_task->sp = stack pointer
+        lea rax, [rax-96]               ; space for pushed registers
+        mov [rcx], rax                  ; new_task->sp = stack pointer
         ret
 
 align 16
@@ -75,8 +75,8 @@ func_return:
 align 16
         ; struct task *task_current(void)
 task_current:
-        mov eax, esp                    ; rax = stack pointer (32-bit)
-        and eax, 0xFFFFE000             ; rax = address of current task
+        mov rax, rsp                    ; rax = stack pointer
+        and rax, -8192                  ; rax = address of current task
         ret
 
 align 16
@@ -129,11 +129,11 @@ task_switch_asm:
         push fs                         ; save segment register fs
         push gs                         ; save segment register gs
 
-        mov eax, esp                    ; rax = stack pointer (32-bit)
+        mov rax, rsp                    ; rax = stack pointer
         test eax, 0x1000                ; test stack pointer
         jz short stack_error
 
-        and eax, 0xFFFFE000             ; rax = address of current task
+        and rax, -8192                  ; rax = address of current task
         cmp dword [rax+20], 2           ; check if task switching is enabled
         jb short task_switch_asm_rsp0
 
@@ -145,7 +145,7 @@ task_switch_asm_rsp0:
         lea rbx, [rcx+0x1FF0]           ; rbx = value of rsp0
         mov [rdx+4], rbx                ; update rsp0 (task-state segment)
 
-        mov [rax], esp                  ; save stack pointer
+        mov [rax], rsp                  ; save stack pointer
         fxsave [rax+0x0C00]             ; save fpu, mmx, and sse state
 
         mov rsp, [rcx]                  ; rsp = next->sp
@@ -181,8 +181,8 @@ stack_error:
 align 16
         ; void task_switch_disable(void)
 task_switch_disable:
-        mov eax, esp                    ; rax = stack pointer (32-bit)
-        and eax, 0xFFFFE000             ; rax = address of current task
+        mov rax, rsp                    ; rax = stack pointer
+        and rax, -8192                  ; rax = address of current task
         add dword [rax+20], 2           ; increment ndisable (ignore bit 0)
         js short state_error
         ret
@@ -191,8 +191,8 @@ align 16
         ; void task_switch_enable(void)
 task_switch_enable:
         push rbp                        ; save register rbp
-        mov eax, esp                    ; rax = stack pointer (32-bit)
-        and eax, 0xFFFFE000             ; rax = address of current task
+        mov rax, rsp                    ; rax = stack pointer
+        and rax, -8192                  ; rax = address of current task
         sub dword [rax+20], 2           ; decrement ndisable (ignore bit 0)
         js short state_error
 
@@ -200,11 +200,11 @@ task_switch_enable:
         jne short .L1
         and dword [rax+20], 0xFFFFFFFE  ; clear bit 0
 
-        mov ebp, esp                    ; save stack pointer
-        and esp, 0xFFFFFFF0             ; align stack
-        sub esp, 32                     ; shadow space
+        mov rbp, rsp                    ; save stack pointer
+        and rsp, -16                    ; align stack
+        sub rsp, 32                     ; shadow space
         call task_yield                 ; switch to another task
-        mov esp, ebp                    ; restore stack pointer
+        mov rsp, rbp                    ; restore stack pointer
 
 .L1:    pop rbp                         ; restore register rpb
         ret
