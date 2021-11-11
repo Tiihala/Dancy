b87b49f421ab48ff91d51eb51b1d392f848bdab6
Antti Tiihala <Tiihala@users.noreply.github.com>
2021-11-11 21:34:08 +0200

    Develop asm.sh
    The which command was not used in a correct way. Some implementations
    print a diagnostic if nothing was located.
    
    Even if it were correct, this new style would be better anyway. I also
    changed the error message for the case if an assembler was not found.

--- scripts/asm.sh
+++ scripts/asm.sh
@@ -10,21 +10,19 @@ fi
 export DANCY_ROOT=`pwd`
 export PATH="$DANCY_ROOT/bin:$DANCY_ROOT/external/bin:$PATH"
 
-ASSEMBLER_BIN=`which nasm || true`
-
-if [ -n "$ASSEMBLER_BIN" ]
+if
+    which nasm > /dev/null 2>&1
 then
     nasm "$@"
     exit 0
 fi
 
-ASSEMBLER_BIN=`which yasm || true`
-
-if [ -n "$ASSEMBLER_BIN" ]
+if
+    which yasm > /dev/null 2>&1
 then
     yasm "$@"
     exit 0
 fi
 
-echo "Error: assembler is not found" 1>&2
+echo "Error: a compatible assembler was not found" 1>&2
 exit 1
