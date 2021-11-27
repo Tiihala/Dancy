3b0ddc10bf4179baa475ff2853c03cdcdcb9945b
Antti Tiihala <Tiihala@users.noreply.github.com>
2021-11-27 20:06:56 +0200

    Develop keyboard.c
    I wanted to use "scan code set 2" and disable the translation in the
    PS/2 controller. However, it seems that these devices are emulated
    more often than not, even though I disabled "USB Legacy Support". It
    is most probably safest to use the "default settings", so I decided
    to continue using the controller translation, i.e. "scan code set 1".
    
    This file contains functions that are not thread-safe, and it has been
    designed so. The "ps2_task" and its initialization are the only ones
    that are allowed call these functions.

--- kernel/ps2/keyboard.c
+++ kernel/ps2/keyboard.c
@@ -19,14 +19,101 @@
 
 #include <dancy.h>
 
+static int probe_state = 0;
+static int kbd_ready = 0;
+
+static const int data_none = -1;
+static int data_led_state = 0x00;
+static int data_scan_code = 0x02;
+static int data_typematic = 0x00;
+
+static int send_command(int command, int data)
+{
+	uint32_t ticks;
+	int resend = 0;
+	int b;
+
+	ps2_send_port1((uint8_t)command);
+
+	ticks = timer_ticks;
+
+	while ((b = ps2_receive_port1()) != 0xFA) {
+		if (b == 0xFE && resend < 32) {
+			ps2_send_port1((uint8_t)command);
+			resend += 1;
+			continue;
+		}
+		if ((timer_ticks - ticks) >= 100)
+			return 1;
+	}
+
+	if (data != data_none) {
+		ps2_send_port1((uint8_t)data);
+
+		ticks = timer_ticks;
+
+		while ((b = ps2_receive_port1()) != 0xFA) {
+			if ((timer_ticks - ticks) >= 100)
+				return 2;
+		}
+	}
+
+	while (ps2_receive_port1() >= 0) { /* void */ }
+
+	return 0;
+}
+
 int ps2_kbd_init(void)
 {
+	/*
+	 * Disable keyboard scanning.
+	 */
+	if (send_command(0xF5, data_none))
+		return 1;
+
+	/*
+	 * Set scan code set 2 (will be translated to code set 1).
+	 */
+	if (send_command(0xF0, data_scan_code))
+		return 1;
+
+	/*
+	 * Set keyboard LEDs.
+	 */
+	if (send_command(0xED, data_led_state))
+		return 1;
+
+	/*
+	 * Set keyboard typematic rate and delay.
+	 */
+	if (send_command(0xF3, data_typematic))
+		return 1;
+
+	/*
+	 * Enable keyboard scanning.
+	 */
+	(void)send_command(0xF4, data_none);
+
+	kbd_ready = 1;
+
 	return 0;
 }
 
 void ps2_kbd_handler(void)
 {
 	int b;
+
+	if (probe_state != 0) {
+		if (probe_state == 1)
+			probe_state = 0;
+		if (probe_state == 2)
+			probe_state = 3;
+	}
+
+	while (kbd_ready == 0) {
+		if (ps2_receive_port1() < 0)
+			return;
+	}
 
 	while ((b = ps2_receive_port1()) >= 0) {
 
@@ -35,5 +122,34 @@ void ps2_kbd_handler(void)
 
 void ps2_kbd_probe(void)
 {
+	/*
+	 * Send an echo command to the keyboard.
+	 */
+	if (probe_state == 0) {
+		ps2_send_port1(0xEE);
+		probe_state = 1;
+		return;
+	}
 
+	/*
+	 * Continue sending the echo commands.
+	 */
+	if (probe_state <= 2) {
+		ps2_send_port1(0xEE);
+		probe_state = 2;
+		kbd_ready = 0;
+		return;
+	}
+
+	/*
+	 * The echo commands were successful or some other bytes
+	 * were received. Try to initialize the keyboard, but go
+	 * back trying the echo commands again if this fails.
+	 */
+	if (ps2_kbd_init()) {
+		probe_state = 2;
+		return;
+	}
+
+	probe_state = 0;
 }
