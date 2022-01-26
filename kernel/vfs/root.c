81252bcbe38c7dd2ca6e0d0c7ec36f7a601d6d4d
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-01-26 17:25:14 +0200

    Create ramfs
    Creating a "FAT 12 or 16" file system image is very easy, especially
    if having a single file allocation table.

--- kernel/vfs/root.c
+++ kernel/vfs/root.c
@@ -20,6 +20,7 @@
 #include <dancy.h>
 
 static struct vfs_node *root_node;
+static void *root_instance;
 
 static int root_id;
 static struct fat_io root_io;
@@ -28,9 +29,55 @@ static int root_get_size(int id, size_t *block_size, size_t *block_total);
 static int root_io_read(int id, size_t lba, size_t *size, void *buf);
 static int root_io_write(int id, size_t lba, size_t *size, const void *buf);
 
+static size_t root_ramfs_size;
+static unsigned char *root_ramfs;
+
+static size_t root_block_size;
+static size_t root_block_total;
+
+static int create_ramfs(void)
+{
+	static const unsigned char parameter_block[] = {
+		/*
+		 * 1 MiB File System (FAT 12).
+		 */
+	DANCY_BLOB(0xEB3C,0x906D,0x6B66,0x732E,0x6661,0x7400,0x1004,0x0100),
+	DANCY_BLOB(0x0180,0x0000,0x01F8,0x0100,0x1000,0x0200,0x0000,0x0000),
+	DANCY_BLOB(0x0000,0x0000,0x8000,0x29C7,0xC6EB,0x2F4E,0x4F20,0x4E41),
+	DANCY_BLOB(0x4D45,0x2020,0x2020,0x4641,0x5431,0x3220,0x2020,0xEBFE)};
+
+	root_ramfs_size = 0x100000;
+	root_ramfs = (unsigned char *)mm_alloc_pages(mm_kernel, 8);
+
+	if (!root_ramfs)
+		return DE_MEMORY;
+
+	memset(root_ramfs, 0, root_ramfs_size);
+	memcpy(&root_ramfs[0], &parameter_block[0], sizeof(parameter_block));
+
+	root_ramfs[0x01FE] = 0x55;
+	root_ramfs[0x01FF] = 0xAA;
+
+	root_ramfs[0x1000] = 0xF8;
+	root_ramfs[0x1001] = 0xFF;
+	root_ramfs[0x1002] = 0xFF;
+
+	root_block_size = 0x1000;
+	root_block_total = 0x100;
+
+	return 0;
+}
+
 int vfs_init_root(struct vfs_node **node)
 {
+	static int run_once;
 	int r;
+
+	if (!spin_trylock(&run_once))
+		return DE_UNEXPECTED;
+
+	if ((r = create_ramfs()) != 0)
+		return r;
 
 	if ((root_node = vfs_alloc_node()) == NULL)
 		return DE_MEMORY;
@@ -42,6 +89,9 @@ int vfs_init_root(struct vfs_node **node)
 	if ((r = fat_io_add(&root_io, &root_id)) != 0)
 		return r;
 
+	if (fat_create(&root_instance, root_id))
+		return DE_MEMORY;
+
 	*node = root_node;
 
 	return 0;
@@ -49,31 +99,35 @@ int vfs_init_root(struct vfs_node **node)
 
 static int root_get_size(int id, size_t *block_size, size_t *block_total)
 {
-	(void)id;
-	(void)block_size;
-	(void)block_total;
+	if (id != root_id)
+		kernel->panic("root_get_size: unexpected behavior");
 
-	return 1;
+	*block_size = root_block_size;
+	*block_total = root_block_total;
+
+	return 0;
 }
 
 static int root_io_read(int id, size_t lba, size_t *size, void *buf)
 {
-	(void)id;
-	(void)lba;
-	(void)buf;
+	size_t offset = lba * root_block_size;
 
-	*size = 0;
+	if (id != root_id)
+		kernel->panic("root_io_read: unexpected behavior");
 
-	return 1;
+	memcpy(buf, &root_ramfs[offset], *size);
+
+	return 0;
 }
 
 static int root_io_write(int id, size_t lba, size_t *size, const void *buf)
 {
-	(void)id;
-	(void)lba;
-	(void)buf;
+	size_t offset = lba * root_block_size;
 
-	*size = 0;
+	if (id != root_id)
+		kernel->panic("root_io_write: unexpected behavior");
 
-	return 1;
+	memcpy(&root_ramfs[offset], buf, *size);
+
+	return 0;
 }
