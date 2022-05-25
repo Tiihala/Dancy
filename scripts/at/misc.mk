da2d8ca726757f3ca50f5d6d44ab89a92da2268d
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-05-25 21:22:52 +0300

    Remove fdn.c
    There should be a generic solution for handling disk block caching.

--- scripts/at/misc.mk
+++ scripts/at/misc.mk
@@ -5,7 +5,6 @@
 DANCY_MISC_OBJECTS_32= \
  ./o32/misc/dma.o \
  ./o32/misc/epoch.o \
- ./o32/misc/fdn.o \
  ./o32/misc/floppy.o \
  ./o32/misc/rtc.o \
  ./o32/misc/serial.o \
@@ -14,7 +13,6 @@ DANCY_MISC_OBJECTS_32= \
 DANCY_MISC_OBJECTS_64= \
  ./o64/misc/dma.o \
  ./o64/misc/epoch.o \
- ./o64/misc/fdn.o \
  ./o64/misc/floppy.o \
  ./o64/misc/rtc.o \
  ./o64/misc/serial.o \
