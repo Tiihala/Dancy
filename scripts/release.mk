18f9fc6f05e9ed9746f98f98f2ee55f628b07a73
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-07-07 18:33:54 +0300

    Update release.mk
    The 64-bit versions have been removed from fdd1440.img.

--- scripts/release.mk
+++ scripts/release.mk
@@ -14,6 +14,8 @@ LOADER_FILE=-t 2019-03-03T08:58:39 --read-only
 	$(DY_MCOPY) -i $@ ./system/DB_000.AT --db
 	$(DY_MCOPY) -i $@ ./system/DB_100.AT --db
 	$(DY_MCOPY) -i $@ ./system/DB_200.AT --db
+	$(DY_MCOPY) -i $@ ./system/DB_300.AT --db
+	$(DY_MCOPY) -i $@ ./system/DB_400.AT --db
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
 	$(DY_MCOPY) -i $@ ./system/IN_X64.AT ::system/IN_X64.AT
 	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
@@ -37,6 +39,7 @@ LOADER_FILE=-t 2019-03-03T08:58:39 --read-only
 	$(DY_MCOPY) -i $@ ./system/CONFIG.AT ::system/CONFIG.AT
 	$(DY_MCOPY) -i $@ ./system/DB_008.AT --db
 	$(DY_MCOPY) -i $@ ./system/DB_100.AT --db
+	$(DY_MCOPY) -i $@ ./system/DB_300.AT --db
 	$(DY_MCOPY) -i $@ ./system/IN_IA16.AT ::system/IN_IA16.AT
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
 	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
@@ -47,10 +50,9 @@ LOADER_FILE=-t 2019-03-03T08:58:39 --read-only
 	$(DY_MCOPY) -i $@ ./system/CONFIG.AT ::system/CONFIG.AT
 	$(DY_MCOPY) -i $@ ./system/DB_000.AT --db
 	$(DY_MCOPY) -i $@ ./system/DB_100.AT --db
-	$(DY_MCOPY) -i $@ ./system/DB_200.AT --db
+	$(DY_MCOPY) -i $@ ./system/DB_300.AT --db
 	$(DY_MCOPY) -i $@ ./system/IN_IA16.AT ::system/IN_IA16.AT
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
-	$(DY_MCOPY) -i $@ ./system/IN_X64.AT ::system/IN_X64.AT
 	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
 	$(DY_MCOPY) -i $@ ./LOADER.AT ::LOADER.AT $(LOADER_FILE)
 
@@ -61,6 +63,8 @@ LOADER_FILE=-t 2019-03-03T08:58:39 --read-only
 	$(DY_MCOPY) -i $@ ./system/DB_000.AT --db
 	$(DY_MCOPY) -i $@ ./system/DB_100.AT --db
 	$(DY_MCOPY) -i $@ ./system/DB_200.AT --db
+	$(DY_MCOPY) -i $@ ./system/DB_300.AT --db
+	$(DY_MCOPY) -i $@ ./system/DB_400.AT --db
 	$(DY_MCOPY) -i $@ ./system/IN_IA32.AT ::system/IN_IA32.AT
 	$(DY_MCOPY) -i $@ ./system/IN_X64.AT ::system/IN_X64.AT
 	$(DY_MCOPY) -i $@ ./LOADER.512 ::LOADER.512 $(LDR512_FILE)
