26b1e6c6754b3a2a2e065eeec02e23c7e5443995
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-07-31 09:49:47 +0300

    Update limits.h
    For aesthetic reasons, error messages are enclosed in double-quotes.

--- include/dancy/limits.h
+++ include/dancy/limits.h
@@ -23,39 +23,39 @@
 #include <limits.h>
 
 #if !defined (CHAR_BIT) || CHAR_BIT != 8
-#error Definition of CHAR_BIT is not compatible
+#error "Definition of CHAR_BIT is not compatible"
 #endif
 
 #if !defined (SCHAR_MIN) || SCHAR_MIN != -128
-#error Definition of SCHAR_MIN is not compatible
+#error "Definition of SCHAR_MIN is not compatible"
 #endif
 
 #if !defined (SCHAR_MAX) || SCHAR_MAX != 127
-#error Definition of SCHAR_MAX is not compatible
+#error "Definition of SCHAR_MAX is not compatible"
 #endif
 
 #if !defined (USHRT_MAX) || USHRT_MAX != 65535u
-#error Definition of USHRT_MAX is not compatible
+#error "Definition of USHRT_MAX is not compatible"
 #endif
 
 #if !defined (UINT_MAX) || UINT_MAX != 4294967295ul
-#error Definition of UINT_MAX is not compatible
+#error "Definition of UINT_MAX is not compatible"
 #endif
 
 #if !defined (ULONG_MAX)
-#error Definition of ULONG_MAX is not available
+#error "Definition of ULONG_MAX is not available"
 #endif
 
 #if defined (DANCY_32) && ULONG_MAX != 4294967295ul
-#error Definition of ULONG_MAX is not compatible
+#error "Definition of ULONG_MAX is not compatible"
 #endif
 
 #if !defined (ULLONG_MAX)
-#error Definition of ULLONG_MAX is not available
+#error "Definition of ULLONG_MAX is not available"
 #endif
 
 #if ULLONG_MAX != 18446744073709551615ull
-#error Definition of ULLONG_MAX is not compatible
+#error "Definition of ULLONG_MAX is not compatible"
 #endif
 
 #endif
