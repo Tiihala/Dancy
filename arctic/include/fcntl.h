46257269ea6de1e081a52e29bbe3ee962c8b9c76
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-10-08 22:25:30 +0300

    Add F_DUPFD_CLOEXEC
    New cmd argument: duplicate file descriptor with flag FD_CLOEXEC set.

--- arctic/include/fcntl.h
+++ arctic/include/fcntl.h
@@ -42,10 +42,11 @@
 #define O_SYNC              0x4000
 
 #define F_DUPFD             0
-#define F_GETFD             1
-#define F_SETFD             2
-#define F_GETFL             3
-#define F_SETFL             4
+#define F_DUPFD_CLOEXEC     1
+#define F_GETFD             2
+#define F_SETFD             3
+#define F_GETFL             4
+#define F_SETFL             5
 
 #define FD_CLOEXEC          0x0001
 
