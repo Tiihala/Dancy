5fb371f54628d82d5cb6bc8b5636c6de436e32ef
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-10-31 20:19:48 +0200

    Add syscall2.asm
    long long __dancy_syscall2(int arg0, ...);

--- scripts/objects/arctic.mk
+++ scripts/objects/arctic.mk
@@ -8,6 +8,9 @@
 ./arctic/o32/libc/a32/start.o: ./arctic/libc/a32/start.asm $(DANCY_DEPS)
 	$(DANCY_A32)$@ ./arctic/libc/a32/start.asm
 
+./arctic/o32/libc/a32/syscall2.o: ./arctic/libc/a32/syscall2.asm $(DANCY_DEPS)
+	$(DANCY_A32)$@ ./arctic/libc/a32/syscall2.asm
+
 ./arctic/o32/libc/a32/syscall3.o: ./arctic/libc/a32/syscall3.asm $(DANCY_DEPS)
 	$(DANCY_A32)$@ ./arctic/libc/a32/syscall3.asm
 
@@ -25,6 +28,9 @@
 
 ./arctic/o64/libc/a64/start.o: ./arctic/libc/a64/start.asm $(DANCY_DEPS)
 	$(DANCY_A64)$@ ./arctic/libc/a64/start.asm
+
+./arctic/o64/libc/a64/syscall2.o: ./arctic/libc/a64/syscall2.asm $(DANCY_DEPS)
+	$(DANCY_A64)$@ ./arctic/libc/a64/syscall2.asm
 
 ./arctic/o64/libc/a64/syscall3.o: ./arctic/libc/a64/syscall3.asm $(DANCY_DEPS)
 	$(DANCY_A64)$@ ./arctic/libc/a64/syscall3.asm
