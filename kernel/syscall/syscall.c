02381b9fbb4c70ebbc2b23b3574cf6c1e59f2ede
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-11-01 21:59:46 +0200

    Develop syscall.c
    I will try to use this approach when calling the system functions. If
    this is hard to maintain, I need to figure out a new strategy.

--- kernel/syscall/syscall.c
+++ kernel/syscall/syscall.c
@@ -20,22 +20,60 @@
 #include <dancy.h>
 #include <errno.h>
 
+static long long dancy_syscall_exit(va_list va)
+{
+	int retval = va_arg(va, int);
+
+	task_exit((int)((unsigned int)retval & 0xFF));
+
+	return kernel->panic("__dancy_syscall_exit: unexpected behavior"), 0;
+}
+
+static long long dancy_syscall_reserved(va_list va)
+{
+	return (void)va, -EINVAL;
+}
+
+static struct { long long (*handler)(va_list va); } handler_array[] = {
+	{ dancy_syscall_exit },
+	{ dancy_syscall_reserved }
+};
+
 int syscall_init(void)
 {
 	static int run_once;
 	extern void syscall_init_asm(void);
+	uint32_t t0, t1, t2, t3;
 
 	if (!spin_trylock(&run_once))
 		return DE_UNEXPECTED;
 
 	syscall_init_asm();
 
+	t0 = (uint32_t)(sizeof( handler_array));
+	t1 = (uint32_t)(sizeof(*handler_array));
+	t2 = (uint32_t)(__dancy_syscall_arg0__);
+	t3 = (uint32_t)(__dancy_syscall_argn__);
+
+	if (cpu_read32(&t0) / t1 != t3 - t2)
+		return DE_UNEXPECTED;
+
 	return 0;
 }
 
 long long syscall_handler(int arg0, ...)
 {
-	(void)arg0;
+	long long r = -EINVAL;
 
-	return -EINVAL;
+	va_list va;
+	va_start(va, arg0);
+
+	if (arg0 > __dancy_syscall_arg0__ && arg0 < __dancy_syscall_argn__) {
+		int i = arg0 - __dancy_syscall_arg0__ - 1;
+		r = handler_array[i].handler(va);
+	}
+
+	va_end(va);
+
+	return r;
 }
