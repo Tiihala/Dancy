eb95429ef159f2114d150528542c0a3d99ab3024
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-12-26 12:10:44 +0200

    Refactor sys/types.h
    The type definitions are in alphabetical order.

--- arctic/include/sys/types.h
+++ arctic/include/sys/types.h
@@ -25,19 +25,9 @@
 
 __Dancy_Header_Begin
 
-#ifndef __DANCY_TYPEDEF_MODE_T
-#define __DANCY_TYPEDEF_MODE_T
-typedef __dancy_mode_t mode_t;
-#endif
-
-#ifndef __DANCY_TYPEDEF_SUSECONDS_T
-#define __DANCY_TYPEDEF_SUSECONDS_T
-typedef __dancy_suseconds_t suseconds_t;
-#endif
-
-#ifndef __DANCY_TYPEDEF_OFF_T
-#define __DANCY_TYPEDEF_OFF_T
-typedef __dancy_off_t off_t;
+#ifndef __DANCY_TYPEDEF_CLOCK_T
+#define __DANCY_TYPEDEF_CLOCK_T
+typedef __dancy_clock_t clock_t;
 #endif
 
 #ifndef __DANCY_TYPEDEF_ID_T
@@ -45,14 +35,24 @@ typedef __dancy_off_t off_t;
 typedef __dancy_id_t id_t;
 #endif
 
+#ifndef __DANCY_TYPEDEF_MODE_T
+#define __DANCY_TYPEDEF_MODE_T
+typedef __dancy_mode_t mode_t;
+#endif
+
+#ifndef __DANCY_TYPEDEF_OFF_T
+#define __DANCY_TYPEDEF_OFF_T
+typedef __dancy_off_t off_t;
+#endif
+
 #ifndef __DANCY_TYPEDEF_PID_T
 #define __DANCY_TYPEDEF_PID_T
 typedef __dancy_pid_t pid_t;
 #endif
 
-#ifndef __DANCY_TYPEDEF_CLOCK_T
-#define __DANCY_TYPEDEF_CLOCK_T
-typedef __dancy_clock_t clock_t;
+#ifndef __DANCY_TYPEDEF_SUSECONDS_T
+#define __DANCY_TYPEDEF_SUSECONDS_T
+typedef __dancy_suseconds_t suseconds_t;
 #endif
 
 #ifndef __DANCY_TYPEDEF_TIME_T
