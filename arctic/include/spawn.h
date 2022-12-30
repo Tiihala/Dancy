c1732da72186b8fb8eeb9f4ea8304b379a0eea00
Antti Tiihala <Tiihala@users.noreply.github.com>
2022-12-30 19:31:16 +0200

    Develop spawn.h
    Now this header should be completely implemented.

--- arctic/include/spawn.h
+++ arctic/include/spawn.h
@@ -21,6 +21,7 @@
 #define __DANCY_SPAWN_H
 
 #include <__dancy/core.h>
+#include <__dancy/sched.h>
 #include <__dancy/spawn.h>
 
 __Dancy_Header_Begin
@@ -33,6 +34,11 @@ typedef __dancy_mode_t mode_t;
 #ifndef __DANCY_TYPEDEF_PID_T
 #define __DANCY_TYPEDEF_PID_T
 typedef __dancy_pid_t pid_t;
+#endif
+
+#ifndef __DANCY_TYPEDEF_SIGSET_T
+#define __DANCY_TYPEDEF_SIGSET_T
+typedef __dancy_sigset_t sigset_t;
 #endif
 
 #define POSIX_SPAWN_RESETIDS        (0x0001)
@@ -53,6 +59,11 @@ int posix_spawn(pid_t *pid, const char *path,
 	const posix_spawnattr_t *attrp,
 	char *const argv[], char *const envp[]);
 
+int posix_spawnp(pid_t *pid, const char *file,
+	const posix_spawn_file_actions_t *actions,
+	const posix_spawnattr_t *attrp,
+	char *const argv[], char *const envp[]);
+
 int posix_spawn_file_actions_init(posix_spawn_file_actions_t *actions);
 int posix_spawn_file_actions_destroy(posix_spawn_file_actions_t *actions);
 
@@ -68,6 +79,36 @@ int posix_spawn_file_actions_addopen(
 
 int posix_spawnattr_init(posix_spawnattr_t *attrp);
 int posix_spawnattr_destroy(posix_spawnattr_t *attrp);
+
+int posix_spawnattr_getflags(const posix_spawnattr_t *attrp, short *flags);
+int posix_spawnattr_setflags(posix_spawnattr_t *attrp, short flags);
+
+int posix_spawnattr_getpgroup(const posix_spawnattr_t *attrp, pid_t *pgroup);
+int posix_spawnattr_setpgroup(posix_spawnattr_t *attrp, pid_t pgroup);
+
+int posix_spawnattr_getschedparam(
+	const posix_spawnattr_t *attrp, struct sched_param *param);
+
+int posix_spawnattr_setschedparam(
+	posix_spawnattr_t *attrp, const struct sched_param *param);
+
+int posix_spawnattr_getschedpolicy(
+	const posix_spawnattr_t *attrp, int *policy);
+
+int posix_spawnattr_setschedpolicy(
+	posix_spawnattr_t *attrp, int policy);
+
+int posix_spawnattr_getsigdefault(
+	const posix_spawnattr_t *attrp, sigset_t *sigdefault);
+
+int posix_spawnattr_setsigdefault(
+	posix_spawnattr_t *attrp, const sigset_t *sigdefault);
+
+int posix_spawnattr_getsigmask(
+	const posix_spawnattr_t *attrp, sigset_t *sigmask);
+
+int posix_spawnattr_setsigmask(
+	posix_spawnattr_t *attrp, const sigset_t *sigmask);
 
 __Dancy_Header_End
 
