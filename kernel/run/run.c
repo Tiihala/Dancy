bdd84ad27d7eed04adde111cca4530c6e17dd357
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-05-20 10:39:28 +0300

    Implement run.c
    The first user-space executable is "/bin/init" and its PID is 1. This
    is a very common convention in Unix-based operating systems, although
    the directory name is often "sbin".

--- kernel/run/run.c
+++ kernel/run/run.c
@@ -19,12 +19,83 @@
 
 #include <dancy.h>
 
+static const char *exe_name = "/bin/init";
+
+static void build_arg_header(cpu_native_t *argh, cpu_native_t size)
+{
+	argh[0] = 1;
+	argh[1] = (cpu_native_t)(&argh[4]);
+	argh[2] = (cpu_native_t)(&argh[6]);
+	argh[3] = size;
+	argh[4] = (cpu_native_t)(&argh[8]);
+	argh[5] = 0;
+	argh[6] = 0;
+	argh[7] = 0;
+
+	strcpy((void *)(&argh[8]), exe_name);
+}
+
+static int open_file_descriptors(void)
+{
+	int fd, r;
+
+	if ((r = file_open(&fd, "/dev/null", O_RDONLY, 0)) != 0)
+		return r;
+
+	if (fd != 0)
+		return DE_UNEXPECTED;
+
+	if ((r = file_open(&fd, "/dev/console", O_WRONLY, 0)) != 0)
+		return r;
+
+	if (fd != 1)
+		return DE_UNEXPECTED;
+
+	if ((r = file_open(&fd, "/dev/console", O_WRONLY, 0)) != 0)
+		return r;
+
+	if (fd != 2)
+		return DE_UNEXPECTED;
+
+	return 0;
+}
+
 int run_init(void)
 {
 	static int run_once;
+	struct vfs_node *node;
+	addr_t user_ip, user_sp;
+	int r;
 
 	if (!spin_trylock(&run_once))
 		return DE_UNEXPECTED;
+
+	if (task_current()->id != 1)
+		return DE_UNEXPECTED;
+
+	if ((r = pg_create()) != 0)
+		return r;
+
+	if ((r = vfs_open(exe_name, &node, 0, 0)) != 0)
+		return (r != DE_NAME) ? r : 0;
+
+	if ((r = coff_load_executable(node, &user_ip)) != 0)
+		return r;
+
+	{
+		const cpu_native_t top = 0x80000000;
+		const cpu_native_t size = 0x100;
+
+		cpu_native_t *argh = (void *)(top - size);
+
+		user_sp = (addr_t)(argh - 1);
+		build_arg_header(argh, size);
+	}
+
+	if ((r = open_file_descriptors()) != 0)
+		return r;
+
+	task_jump(user_ip, user_sp);
 
 	return 0;
 }
