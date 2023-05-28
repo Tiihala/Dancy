0423970e09ec297631be7ead7986a288097351f5
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-05-28 09:40:02 +0300

    Fix atexit.c

--- arctic/libc/stdlib/atexit.c
+++ arctic/libc/stdlib/atexit.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Antti Tiihala
+ * Copyright (c) 2023 Antti Tiihala
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
