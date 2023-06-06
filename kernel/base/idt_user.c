634db5a41d732f7fd27cd9ce7b60cf1783af1b13
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-06-06 19:11:05 +0300

    Develop idt_user.c
    Normal exceptions must not crash the whole operating system.

--- kernel/base/idt_user.c
+++ kernel/base/idt_user.c
@@ -24,6 +24,90 @@ int idt_user_exception(int num, void *stack)
 	const cpu_native_t *p = stack;
 
 	/*
+	 * Divide-by-Zero Exception
+	 */
+	if (num == 0)
+		task_exit(SIGFPE);
+
+	/*
+	 * Debug Exception
+	 */
+	if (num == 1)
+		task_exit(SIGILL);
+
+	/*
+	 * Non-Maskable-Interrupt Exception
+	 */
+	if (num == 2)
+		return 1;
+
+	/*
+	 * Breakpoint Exception
+	 */
+	if (num == 3)
+		task_exit(SIGILL);
+
+	/*
+	 * Overflow Exception
+	 */
+	if (num == 4)
+		task_exit(SIGFPE);
+
+	/*
+	 * Bound-Range Exception
+	 */
+	if (num == 5)
+		task_exit(SIGSEGV);
+
+	/*
+	 * Invalid-Opcode Exception
+	 */
+	if (num == 6)
+		task_exit(SIGILL);
+
+	/*
+	 * Device-Not-Available Exception
+	 */
+	if (num == 7)
+		task_exit(SIGFPE);
+
+	/*
+	 * Double-Fault Exception
+	 */
+	if (num == 8)
+		return 1;
+
+	/*
+	 * Coprocessor-Segment-Overrun Exception
+	 */
+	if (num == 9)
+		task_exit(SIGFPE);
+
+	/*
+	 * Invalid-TSS Exception
+	 */
+	if (num == 10)
+		task_exit(SIGILL);
+
+	/*
+	 * Segment-Not-Present Exception
+	 */
+	if (num == 11)
+		task_exit(SIGILL);
+
+	/*
+	 * Stack Exception
+	 */
+	if (num == 12)
+		task_exit(SIGSEGV);
+
+	/*
+	 * General-Protection Exception
+	 */
+	if (num == 13)
+		task_exit(SIGILL);
+
+	/*
 	 * Page-Fault Exception
 	 */
 	if (num == 14) {
