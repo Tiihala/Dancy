bab68fc17125ac4c3b88b6e20afebb0c04b92fc7
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-07-02 18:38:45 +0300

    Update boot scripts
    The object hierarchy has been changed.

--- scripts/boot/init.mk
+++ scripts/boot/init.mk
@@ -3,44 +3,44 @@
 ##############################################################################
 
 DANCY_START_OBJECTS_32= \
- ./o32/_boot/init/a32/start.o \
- ./o32/_boot/init/a32/syscall.o \
- ./o32/_boot/init/db.o \
- ./o32/_boot/init/ld.o \
- ./o32/_boot/init/memory.o \
- ./o32/_boot/init/print.o \
- ./o32/_boot/init/start.o \
- ./o32/_common/a32/dark.o \
- ./o32/_common/a32/string.o \
- ./o32/_common/crc32.o \
- ./o32/_common/crc32c.o \
- ./o32/lib/a32/coff.o \
- ./o32/lib/a32/spin.o \
- ./o32/lib/bitarray.o \
- ./o32/lib/huffman.o \
- ./o32/lib/inflate.o \
- ./o32/lib/snprintf.o \
- ./o32/lib/string.o \
+ ./o32/boot/init/a32/start.o \
+ ./o32/boot/init/a32/syscall.o \
+ ./o32/boot/init/db.o \
+ ./o32/boot/init/ld.o \
+ ./o32/boot/init/memory.o \
+ ./o32/boot/init/print.o \
+ ./o32/boot/init/start.o \
+ ./o32/common/a32/dark.o \
+ ./o32/common/a32/string.o \
+ ./o32/common/crc32.o \
+ ./o32/common/crc32c.o \
+ ./o32/kernel/lib/a32/coff.o \
+ ./o32/kernel/lib/a32/spin.o \
+ ./o32/kernel/lib/bitarray.o \
+ ./o32/kernel/lib/huffman.o \
+ ./o32/kernel/lib/inflate.o \
+ ./o32/kernel/lib/snprintf.o \
+ ./o32/kernel/lib/string.o \
 
 DANCY_START_OBJECTS_64= \
- ./o64/_boot/init/a64/start.o \
- ./o64/_boot/init/a64/syscall.o \
- ./o64/_boot/init/db.o \
- ./o64/_boot/init/ld.o \
- ./o64/_boot/init/memory.o \
- ./o64/_boot/init/print.o \
- ./o64/_boot/init/start.o \
- ./o64/_common/a64/dark.o \
- ./o64/_common/a64/string.o \
- ./o64/_common/crc32.o \
- ./o64/_common/crc32c.o \
- ./o64/lib/a64/coff.o \
- ./o64/lib/a64/spin.o \
- ./o64/lib/bitarray.o \
- ./o64/lib/huffman.o \
- ./o64/lib/inflate.o \
- ./o64/lib/snprintf.o \
- ./o64/lib/string.o \
+ ./o64/boot/init/a64/start.o \
+ ./o64/boot/init/a64/syscall.o \
+ ./o64/boot/init/db.o \
+ ./o64/boot/init/ld.o \
+ ./o64/boot/init/memory.o \
+ ./o64/boot/init/print.o \
+ ./o64/boot/init/start.o \
+ ./o64/common/a64/dark.o \
+ ./o64/common/a64/string.o \
+ ./o64/common/crc32.o \
+ ./o64/common/crc32c.o \
+ ./o64/kernel/lib/a64/coff.o \
+ ./o64/kernel/lib/a64/spin.o \
+ ./o64/kernel/lib/bitarray.o \
+ ./o64/kernel/lib/huffman.o \
+ ./o64/kernel/lib/inflate.o \
+ ./o64/kernel/lib/snprintf.o \
+ ./o64/kernel/lib/string.o \
 
 ##############################################################################
 
@@ -55,69 +55,69 @@ DANCY_START_OBJECTS_64= \
 ##############################################################################
 
 DANCY_INIT_OBJECTS_32= \
- ./o32/_boot/init/a32/gdt.o \
- ./o32/_boot/init/a32/idt.o \
- ./o32/_boot/init/a32/kernel.o \
- ./o32/_boot/init/smp/smp.o \
- ./o32/_boot/init/smp/start32.o \
- ./o32/_boot/init/acpi.o \
- ./o32/_boot/init/apic.o \
- ./o32/_boot/init/cpu.o \
- ./o32/_boot/init/delay.o \
- ./o32/_boot/init/fs.o \
- ./o32/_boot/init/gui.o \
- ./o32/_boot/init/hpet.o \
- ./o32/_boot/init/idt.o \
- ./o32/_boot/init/init.o \
- ./o32/_boot/init/kernel.o \
- ./o32/_boot/init/log.o \
- ./o32/_boot/init/panic.o \
- ./o32/_boot/init/pci.o \
- ./o32/_boot/init/pg.o \
- ./o32/_boot/init/pit.o \
- ./o32/_boot/init/rtc.o \
- ./o32/_boot/init/table.o \
- ./o32/_boot/init/usb.o \
- ./o32/_boot/init/vga.o \
- ./o32/_common/fat.o \
- ./o32/_common/ttf.o \
- ./o32/lib/a32/cpu.o \
- ./o32/lib/stdlib.o \
+ ./o32/boot/init/a32/gdt.o \
+ ./o32/boot/init/a32/idt.o \
+ ./o32/boot/init/a32/kernel.o \
+ ./o32/boot/init/smp/smp.o \
+ ./o32/boot/init/smp/start32.o \
+ ./o32/boot/init/acpi.o \
+ ./o32/boot/init/apic.o \
+ ./o32/boot/init/cpu.o \
+ ./o32/boot/init/delay.o \
+ ./o32/boot/init/fs.o \
+ ./o32/boot/init/gui.o \
+ ./o32/boot/init/hpet.o \
+ ./o32/boot/init/idt.o \
+ ./o32/boot/init/init.o \
+ ./o32/boot/init/kernel.o \
+ ./o32/boot/init/log.o \
+ ./o32/boot/init/panic.o \
+ ./o32/boot/init/pci.o \
+ ./o32/boot/init/pg.o \
+ ./o32/boot/init/pit.o \
+ ./o32/boot/init/rtc.o \
+ ./o32/boot/init/table.o \
+ ./o32/boot/init/usb.o \
+ ./o32/boot/init/vga.o \
+ ./o32/common/fat.o \
+ ./o32/common/ttf.o \
+ ./o32/kernel/lib/a32/cpu.o \
+ ./o32/kernel/lib/stdlib.o \
 
 DANCY_INIT_OBJECTS_64= \
- ./o64/_boot/init/a64/gdt.o \
- ./o64/_boot/init/a64/idt.o \
- ./o64/_boot/init/a64/kernel.o \
- ./o64/_boot/init/smp/smp.o \
- ./o64/_boot/init/smp/start64.o \
- ./o64/_boot/init/acpi.o \
- ./o64/_boot/init/apic.o \
- ./o64/_boot/init/cpu.o \
- ./o64/_boot/init/delay.o \
- ./o64/_boot/init/fs.o \
- ./o64/_boot/init/gui.o \
- ./o64/_boot/init/hpet.o \
- ./o64/_boot/init/idt.o \
- ./o64/_boot/init/init.o \
- ./o64/_boot/init/kernel.o \
- ./o64/_boot/init/log.o \
- ./o64/_boot/init/panic.o \
- ./o64/_boot/init/pci.o \
- ./o64/_boot/init/pg.o \
- ./o64/_boot/init/pit.o \
- ./o64/_boot/init/rtc.o \
- ./o64/_boot/init/table.o \
- ./o64/_boot/init/usb.o \
- ./o64/_boot/init/vga.o \
- ./o64/_common/fat.o \
- ./o64/_common/ttf.o \
- ./o64/lib/a64/cpu.o \
- ./o64/lib/stdlib.o \
+ ./o64/boot/init/a64/gdt.o \
+ ./o64/boot/init/a64/idt.o \
+ ./o64/boot/init/a64/kernel.o \
+ ./o64/boot/init/smp/smp.o \
+ ./o64/boot/init/smp/start64.o \
+ ./o64/boot/init/acpi.o \
+ ./o64/boot/init/apic.o \
+ ./o64/boot/init/cpu.o \
+ ./o64/boot/init/delay.o \
+ ./o64/boot/init/fs.o \
+ ./o64/boot/init/gui.o \
+ ./o64/boot/init/hpet.o \
+ ./o64/boot/init/idt.o \
+ ./o64/boot/init/init.o \
+ ./o64/boot/init/kernel.o \
+ ./o64/boot/init/log.o \
+ ./o64/boot/init/panic.o \
+ ./o64/boot/init/pci.o \
+ ./o64/boot/init/pg.o \
+ ./o64/boot/init/pit.o \
+ ./o64/boot/init/rtc.o \
+ ./o64/boot/init/table.o \
+ ./o64/boot/init/usb.o \
+ ./o64/boot/init/vga.o \
+ ./o64/common/fat.o \
+ ./o64/common/ttf.o \
+ ./o64/kernel/lib/a64/cpu.o \
+ ./o64/kernel/lib/stdlib.o \
 
 ##############################################################################
 
-./o32/_boot/init.at: $(DANCY_INIT_OBJECTS_32)
+./o32/boot/init.at: $(DANCY_INIT_OBJECTS_32)
 	$(DY_LINK) -o$@ -fat $(DANCY_INIT_OBJECTS_32)
 
-./o64/_boot/init.at: $(DANCY_INIT_OBJECTS_64)
+./o64/boot/init.at: $(DANCY_INIT_OBJECTS_64)
 	$(DY_LINK) -o$@ -fat $(DANCY_INIT_OBJECTS_64)
--- scripts/boot/uefi.mk
+++ scripts/boot/uefi.mk
@@ -3,36 +3,36 @@
 ##############################################################################
 
 BOOTX64_EFI_OBJECTS= \
- ./o64/_boot/uefi/a64/cpu.o \
- ./o64/_boot/uefi/a64/file.o \
- ./o64/_boot/uefi/a64/font7x9.o \
- ./o64/_boot/uefi/a64/memory.o \
- ./o64/_boot/uefi/a64/pic.o \
- ./o64/_boot/uefi/a64/start.o \
- ./o64/_boot/uefi/a64/syscall.o \
- ./o64/_boot/uefi/block.o \
- ./o64/_boot/uefi/file.o \
- ./o64/_boot/uefi/key.o \
- ./o64/_boot/uefi/log.o \
- ./o64/_boot/uefi/memory.o \
- ./o64/_boot/uefi/misc.o \
- ./o64/_boot/uefi/print.o \
- ./o64/_boot/uefi/serial.o \
- ./o64/_boot/uefi/syscall.o \
- ./o64/_boot/uefi/uefi.o \
- ./o64/_boot/uefi/video.o \
- ./o64/_common/a64/dark.o \
- ./o64/_common/a64/string.o \
- ./o64/_common/crc32c.o \
- ./o64/lib/ctype.o \
- ./o64/lib/snprintf.o \
- ./o64/lib/stdlib.o \
- ./o64/lib/string.o \
+ ./o64/boot/uefi/a64/cpu.o \
+ ./o64/boot/uefi/a64/file.o \
+ ./o64/boot/uefi/a64/font7x9.o \
+ ./o64/boot/uefi/a64/memory.o \
+ ./o64/boot/uefi/a64/pic.o \
+ ./o64/boot/uefi/a64/start.o \
+ ./o64/boot/uefi/a64/syscall.o \
+ ./o64/boot/uefi/block.o \
+ ./o64/boot/uefi/file.o \
+ ./o64/boot/uefi/key.o \
+ ./o64/boot/uefi/log.o \
+ ./o64/boot/uefi/memory.o \
+ ./o64/boot/uefi/misc.o \
+ ./o64/boot/uefi/print.o \
+ ./o64/boot/uefi/serial.o \
+ ./o64/boot/uefi/syscall.o \
+ ./o64/boot/uefi/uefi.o \
+ ./o64/boot/uefi/video.o \
+ ./o64/common/a64/dark.o \
+ ./o64/common/a64/string.o \
+ ./o64/common/crc32c.o \
+ ./o64/kernel/lib/ctype.o \
+ ./o64/kernel/lib/snprintf.o \
+ ./o64/kernel/lib/stdlib.o \
+ ./o64/kernel/lib/string.o \
 
 ##############################################################################
 
-./efi/boot/BOOTX64.EFI: ./o64/_boot/uefi.bin
-	$(DY_UEFI) -o$@ -tx64 ./o64/_boot/uefi.bin
+./efi/boot/BOOTX64.EFI: ./o64/boot/uefi.bin
+	$(DY_UEFI) -o$@ -tx64 ./o64/boot/uefi.bin
 
-./o64/_boot/uefi.bin: $(BOOTX64_EFI_OBJECTS)
+./o64/boot/uefi.bin: $(BOOTX64_EFI_OBJECTS)
 	$(DY_LINK) -o$@ -fuefi $(BOOTX64_EFI_OBJECTS)
