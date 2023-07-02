c43a7ac5b75c365cf3a052f0f1495322f4ed1e2d
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-07-02 19:17:32 +0300

    Update acpios.mk

--- scripts/at/acpios.mk
+++ scripts/at/acpios.mk
@@ -3,12 +3,12 @@
 ##############################################################################
 
 ACPIOS_OBJECTS_32= \
- ./o32/acpios/a32/glblock.o \
- ./o32/acpios/osdancy.o \
+ ./o32/kernel/acpios/a32/glblock.o \
+ ./o32/kernel/acpios/osdancy.o \
 
 ACPIOS_OBJECTS_64= \
- ./o64/acpios/a64/glblock.o \
- ./o64/acpios/osdancy.o \
+ ./o64/kernel/acpios/a64/glblock.o \
+ ./o64/kernel/acpios/osdancy.o \
 
 ##############################################################################
 
@@ -20,14 +20,18 @@ ACPIOS_OBJECTS_64= \
 
 ##############################################################################
 
-./o32/acpios/a32/glblock.o: ./kernel/acpios/a32/glblock.asm $(ACPICA_DEPS)
+./o32/kernel/acpios/a32/glblock.o: \
+    ./kernel/acpios/a32/glblock.asm $(ACPICA_DEPS)
 	$(DANCY_A32)$@ ./kernel/acpios/a32/glblock.asm
 
-./o32/acpios/osdancy.o: ./kernel/acpios/osdancy.c $(ACPICA_DEPS)
+./o32/kernel/acpios/osdancy.o: \
+    ./kernel/acpios/osdancy.c $(ACPICA_DEPS)
 	$(ACPICA_O32)$@ ./kernel/acpios/osdancy.c
 
-./o64/acpios/a64/glblock.o: ./kernel/acpios/a64/glblock.asm $(ACPICA_DEPS)
+./o64/kernel/acpios/a64/glblock.o: \
+    ./kernel/acpios/a64/glblock.asm $(ACPICA_DEPS)
 	$(DANCY_A64)$@ ./kernel/acpios/a64/glblock.asm
 
-./o64/acpios/osdancy.o: ./kernel/acpios/osdancy.c $(ACPICA_DEPS)
+./o64/kernel/acpios/osdancy.o: \
+    ./kernel/acpios/osdancy.c $(ACPICA_DEPS)
 	$(ACPICA_O64)$@ ./kernel/acpios/osdancy.c
