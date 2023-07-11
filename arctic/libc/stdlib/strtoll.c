406a6451990fcdd9e51acb135aea9101d56ac8d6
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-07-11 21:48:33 +0300

    Add strtoll.c
    long long strtoll(const char *str, char **endptr, int base);

---
+++ arctic/libc/stdlib/strtoll.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023 Antti Tiihala
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * libc/stdlib/strtoll.c
+ *      The C Standard Library
+ */
+
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+
+long long strtoll(const char *str, char **endptr, int base)
+{
+	unsigned long long r = 0;
+	char *end = NULL;
+	int sign = 0;
+	int r_errno;
+
+	if (endptr != NULL)
+		*endptr = (char *)str;
+
+	while (isspace((int)(*str)))
+		str += 1;
+
+	if (*str == '+' || *str == '-')
+		sign = (int)(*str++);
+
+	r_errno = __dancy_ull(str, &end, base, &r);
+
+	if (r_errno == EINVAL)
+		return (errno = r_errno), 0;
+
+	if (endptr != NULL)
+		*endptr = end;
+
+	if (r_errno == ERANGE) {
+		long long m = ((sign == '-') ? LLONG_MIN : LLONG_MAX);
+		return (errno = r_errno), m;
+	}
+
+	if (sign == '-') {
+		long long m = LLONG_MIN;
+
+		if (r > (unsigned long long)(-(m + 1LL)) + 1ULL)
+			return (errno = ERANGE), m;
+
+		if (r == (unsigned long long)(-(m + 1LL)) + 1ULL)
+			return m;
+
+		return -((long long)r);
+	}
+
+	if (r > (unsigned long long)(LLONG_MAX))
+		return (errno = ERANGE), LLONG_MAX;
+
+	return (long long)r;
+}
