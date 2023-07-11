152036ccd2fb21f4ef23c409618f808ec4042287
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-07-11 21:45:46 +0300

    Add strtol.c
    long strtol(const char *str, char **endptr, int base);

---
+++ arctic/libc/stdlib/strtol.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023 Antti Tiihala
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * libc/stdlib/strtol.c
+ *      The C Standard Library
+ */
+
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+
+long strtol(const char *str, char **endptr, int base)
+{
+	unsigned long long r = 0;
+	char *end = NULL;
+	int sign = 0;
+	int r_errno;
+
+	if (endptr != NULL)
+		*endptr = (char *)str;
+
+	while (isspace((int)(*str)))
+		str += 1;
+
+	if (*str == '+' || *str == '-')
+		sign = (int)(*str++);
+
+	r_errno = __dancy_ull(str, &end, base, &r);
+
+	if (r_errno == EINVAL)
+		return (errno = r_errno), 0;
+
+	if (endptr != NULL)
+		*endptr = end;
+
+	if (r_errno == ERANGE) {
+		long m = ((sign == '-') ? LONG_MIN : LONG_MAX);
+		return (errno = r_errno), m;
+	}
+
+	if (sign == '-') {
+		long m = LONG_MIN;
+
+		if (r > (unsigned long long)(-(m + 1L)) + 1ULL)
+			return (errno = ERANGE), m;
+
+		if (r == (unsigned long long)(-(m + 1L)) + 1ULL)
+			return m;
+
+		return -((long)r);
+	}
+
+	if (r > (unsigned long long)(LONG_MAX))
+		return (errno = ERANGE), LONG_MAX;
+
+	return (long)r;
+}
