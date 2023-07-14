454289cb305f8a7b23c765226163bc868a3fc18a
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-07-14 19:42:55 +0300

    Fix sys/select.h
    void FD_ZERO(fd_set *set);

--- arctic/include/sys/select.h
+++ arctic/include/sys/select.h
@@ -45,7 +45,7 @@ typedef struct __dancy_fd_set fd_set;
 void FD_CLR(int fd, fd_set *set);
 int FD_ISSET(int fd, fd_set *set);
 void FD_SET(int fd, fd_set *set);
-void FD_ZERO(int fd, fd_set *set);
+void FD_ZERO(fd_set *set);
 
 int select(int nfds,
 	fd_set *readfds, fd_set *writefds, fd_set *errorfds,
