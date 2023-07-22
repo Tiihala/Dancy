fb8b3cf40e6548a44bbe4656c049fbeedabebfc3
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-07-22 12:09:57 +0300

    Refactor syscall.h
    The order of the declarations has been changed.

--- include/kernel/syscall.h
+++ include/kernel/syscall.h
@@ -61,14 +61,14 @@ int file_realpath(const char *name, void *buffer, size_t size);
 int file_poll(struct pollfd fds[], int nfds, int timeout, int *retval);
 
 /*
- * Declarations of kill.c
- */
-int kill_internal(__dancy_pid_t pid, int sig, int flags);
-
-/*
  * Declarations of ioctl.c
  */
 int ioctl_internal(int fd, int request, long long arg, long long *retval);
+
+/*
+ * Declarations of kill.c
+ */
+int kill_internal(__dancy_pid_t pid, int sig, int flags);
 
 /*
  * Declarations of sleep.c
