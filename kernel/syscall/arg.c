13b77ba2210de93649132c1fc48b217091523255
Antti Tiihala <Tiihala@users.noreply.github.com>
2023-08-27 22:37:47 +0300

    Decrease initial stack size
    The user space stack can grow dynamically, so it is not necessary to
    allocate too much initially.

--- kernel/syscall/arg.c
+++ kernel/syscall/arg.c
@@ -165,8 +165,8 @@ int arg_copy(void *arg_state, addr_t *user_sp)
 	ah->size = 0;
 
 	{
-		addr_t map_base = (addr_t)(base - 0x8000);
-		size_t map_size = (size_t)(size + 0x8000);
+		addr_t map_base = (addr_t)(base - 0x2000);
+		size_t map_size = (size_t)(size + 0x2000);
 
 		if (!pg_map_user(map_base, map_size))
 			return DE_MEMORY;
