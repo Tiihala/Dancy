41dcf1589027550e905000dfd9f5ad38d60f5098
Antti Tiihala <Tiihala@users.noreply.github.com>
2024-03-19 18:49:26 +0200

    Develop programs/init
    A lot of general improvements, e.g. using the clock_nanosleep function
    for preventing too fast error printing.

--- arctic/programs/init/main.h
+++ arctic/programs/init/main.h
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 struct options {
--- arctic/programs/init/operate.c
+++ arctic/programs/init/operate.c
@@ -19,12 +19,10 @@
 
 #include "main.h"
 
+static const char *msg_begin = "\n\n\033[97m", *msg_end = "\033[0m\n\n";
+
 static int check_status(const char *path, int status)
 {
-	const char *begin = "\n\n\033[97m", *end = "\033[0m\n\n";
-
-	const char *s0 = "restarting the Dancy Shell";
-	const char *s1 = "please restart the computer";
 	int exit_code = 0;
 
 	if (WIFEXITED(status))
@@ -33,10 +31,10 @@ static int check_status(const char *path, int status)
 	if (WIFSIGNALED(status))
 		exit_code = 128 + WTERMSIG(status);
 
-	fprintf(stderr, "%sinit: \'%s\' exited with code %d\ninit: %s%s\n",
-		begin, path, exit_code, (exit_code < 128) ? s0 : s1, end);
+	fprintf(stderr, "%sinit: \'%s\' exited with code %d\n%s",
+		msg_begin, path, exit_code, msg_end);
 
-	return !(exit_code < 128);
+	return exit_code;
 }
 
 int operate(struct options *opt)
@@ -54,15 +52,23 @@ int operate(struct options *opt)
 	}
 
 	for (;;) {
+		struct timespec sleep_request = { 1, 0 };
 		const char *path = "/bin/terminal";
 		const char *a[] = { path, "/bin/dsh", NULL };
 		pid_t new_pid = -1;
 		int status = -1;
 
-		r = posix_spawn(&new_pid, path, NULL, NULL, (char **)a, NULL);
+		posix_spawnattr_t at;
+		posix_spawnattr_init(&at);
+		posix_spawnattr_setflags(&at, POSIX_SPAWN_SETSID);
 
-		if (r != 0 || new_pid < 0)
+		r = posix_spawn(&new_pid, path, NULL, &at, (char **)a, NULL);
+
+		if (r != 0 || new_pid < 0) {
+			fprintf(stderr, "%sinit: \'%s\' failed (%s)\n%s",
+				msg_begin, path, strerror(r), msg_end);
 			break;
+		}
 
 		for (;;) {
 			pid_t pid = waitpid(-1, &status, 0);
@@ -71,8 +77,17 @@ int operate(struct options *opt)
 				break;
 		}
 
+		for (;;) {
+			pid_t pid = waitpid(-1, NULL, WNOHANG);
+
+			if (pid <= 0)
+				break;
+		}
+
 		if (check_status(path, status))
-			break;
+			sleep_request.tv_sec = 4;
+
+		clock_nanosleep(CLOCK_MONOTONIC, 0, &sleep_request, NULL);
 	}
 
 	return 0;
