2df6ed01faad33c7ce69c1e2ec299ee924c17d91
Antti Tiihala <Tiihala@users.noreply.github.com>
2024-05-05 16:57:21 +0300

    Update external.sh
    The default x86_64-w64-mingw32-gcc package is not compatible.

--- scripts/external.sh
+++ scripts/external.sh
@@ -37,8 +37,6 @@ GCC_AVAILABLE=0
 which nasm > /dev/null 2>&1 && ASM_AVAILABLE=1
 which yasm > /dev/null 2>&1 && ASM_AVAILABLE=1
 
-which x86_64-w64-mingw32-gcc > /dev/null 2>&1 && GCC_AVAILABLE=1
-
 if [ $ASM_AVAILABLE -eq 0 ]
 then
     echo -e "\e[33mBuilding an assembler:"
