67188a17b30c2426d820a70115ce04c30c66d3e1
Antti Tiihala <Tiihala@users.noreply.github.com>
2024-12-31 17:18:59 +0200

    Increase ramfs sizes
    The sizes of architecture-specific images have been changed.

--- scripts/arctic/_bin32.mk
+++ scripts/arctic/_bin32.mk
@@ -29,7 +29,7 @@ ARCTIC_BIN32_FILES= \
 ##############################################################################
 
 ./arctic/bin32.img: $(ARCTIC_BIN32_FILES)
-	$(DY_VBR) -t ramfs $@ 720
+	$(DY_VBR) -t ramfs $@ 2880
 	$(DY_MCOPY) -i $@ ./arctic/bin32/hello ::hello
 	$(DY_MCOPY) -i $@ ./arctic/bin32/cat ::cat
 	$(DY_MCOPY) -i $@ ./arctic/bin32/cp ::cp
--- scripts/arctic/_bin64.mk
+++ scripts/arctic/_bin64.mk
@@ -29,7 +29,7 @@ ARCTIC_BIN64_FILES= \
 ##############################################################################
 
 ./arctic/bin64.img: $(ARCTIC_BIN64_FILES)
-	$(DY_VBR) -t ramfs $@ 720
+	$(DY_VBR) -t ramfs $@ 2880
 	$(DY_MCOPY) -i $@ ./arctic/bin64/hello ::hello
 	$(DY_MCOPY) -i $@ ./arctic/bin64/cat ::cat
 	$(DY_MCOPY) -i $@ ./arctic/bin64/cp ::cp
