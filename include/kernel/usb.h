70ad77745e3d77ffa4a503e25d6f9063fbbcb8a7
Antti Tiihala <Tiihala@users.noreply.github.com>
2025-02-28 14:27:16 +0200

    Develop usb.h
    Some descriptor structures have been added. Also, a structure for the
    device drivers, but this one may require more planning.

--- include/kernel/usb.h
+++ include/kernel/usb.h
@@ -30,6 +30,46 @@ struct usb_device_request {
 	uint16_t wLength;
 };
 
+struct usb_device_descriptor {
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint16_t bcdUSB;
+	uint8_t bDeviceClass;
+	uint8_t bDeviceSubClass;
+	uint8_t bDeviceProtocol;
+	uint8_t bMaxPacketSize0;
+	uint16_t idVendor;
+	uint16_t idProduct;
+	uint8_t bcdDevice;
+	uint8_t iManufacturer;
+	uint8_t iProduct;
+	uint8_t iSerialNumber;
+	uint8_t bNumConfigurations;
+};
+
+struct usb_configuration_descriptor {
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint16_t wTotalLength;
+	uint8_t bNumInterfaces;
+	uint8_t bConfigurationValue;
+	uint8_t iConfiguration;
+	uint8_t bmAttributes;
+	uint8_t bMaxPower;
+};
+
+struct usb_interface_descriptor {
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint8_t bInterfaceNumber;
+	uint8_t bAlternateSetting;
+	uint8_t bNumEndpoints;
+	uint8_t bInterfaceClass;
+	uint8_t bInterfaceSubClass;
+	uint8_t bInterfaceProtocol;
+	uint8_t iInterface;
+};
+
 struct usb_endpoint_descriptor {
 	uint8_t bLength;
 	uint8_t bDescriptorType;
@@ -76,6 +116,16 @@ struct dancy_usb_node {
 
 	int port;
 	int device;
+};
+
+struct dancy_usb_driver {
+	struct {
+		void *_buffer;
+		struct usb_device_descriptor *device;
+		struct usb_configuration_descriptor *configuration;
+		struct usb_interface_descriptor *interface;
+		struct usb_endpoint_descriptor *endpoints[32];
+	} descriptor;
 };
 
 /*
