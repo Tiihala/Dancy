82b034cd1e0279e029214b3a18e99c74655f0be3
Antti Tiihala <Tiihala@users.noreply.github.com>
2025-04-19 19:09:03 +0300

    Develop usb_mnt.c
    I was able to read and write files. What a great milestone!

--- kernel/usb/usb_mnt.c
+++ kernel/usb/usb_mnt.c
@@ -19,7 +19,209 @@
 
 #include <dancy.h>
 
+static int usb_mnt_lock;
+struct vfs_node *usb_mnt_nodes[6];
+
+struct usb_mnt_internal_data {
+	struct vfs_node *msc_node;
+	int lock;
+	int media_changed;
+};
+
+static int mount_drive(const char *name, struct vfs_node *mnt_node)
+{
+	struct vfs_node *node;
+	int mode = vfs_mode_create;
+	int r;
+
+	if ((r = vfs_open("/mnt/", &node, 0, mode)) != 0)
+		return r;
+
+	node->n_release(&node);
+
+	if ((r = vfs_open(name, &node, vfs_type_directory, mode)) != 0)
+		return r;
+
+	node->n_release(&node);
+
+	if ((r = fat_io_create(&node, mnt_node)) != 0) {
+		vfs_unlink(name);
+		return r;
+	}
+
+	if ((r = vfs_mount(name, node)) != 0)
+		vfs_unlink(name);
+
+	node->n_release(&node);
+
+	return r;
+}
+
+static int n_read(struct vfs_node *node,
+	uint64_t offset, size_t *size, void *buffer)
+{
+	struct usb_mnt_internal_data *data = node->internal_data;
+
+	spin_lock_yield(&data->lock);
+
+	if (data->msc_node != NULL) {
+		struct vfs_node *msc_node = data->msc_node;
+		int r = msc_node->n_read(msc_node, offset, size, buffer);
+
+		if (r == DE_MEDIA_CHANGED)
+			data->media_changed = 1;
+
+		return spin_unlock(&data->lock), r;
+	}
+
+	return *size = 0, spin_unlock(&data->lock), 0;
+}
+
+static int n_write(struct vfs_node *node,
+	uint64_t offset, size_t *size, const void *buffer)
+{
+	struct usb_mnt_internal_data *data = node->internal_data;
+
+	spin_lock_yield(&data->lock);
+
+	if (data->msc_node != NULL) {
+		struct vfs_node *msc_node = data->msc_node;
+		int r = msc_node->n_write(msc_node, offset, size, buffer);
+
+		if (r == DE_MEDIA_CHANGED)
+			data->media_changed = 1;
+
+		return spin_unlock(&data->lock), r;
+	}
+
+	return *size = 0, spin_unlock(&data->lock), 0;
+}
+
+static int n_sync(struct vfs_node *node)
+{
+	struct usb_mnt_internal_data *data = node->internal_data;
+
+	spin_lock_yield(&data->lock);
+
+	if (data->msc_node != NULL) {
+		struct vfs_node *msc_node = data->msc_node;
+		int r = msc_node->n_sync(msc_node);
+
+		if (r == DE_MEDIA_CHANGED)
+			data->media_changed = 1;
+
+		return spin_unlock(&data->lock), r;
+	}
+
+	return spin_unlock(&data->lock), 0;
+}
+
+static int n_stat(struct vfs_node *node, struct vfs_stat *stat)
+{
+	struct usb_mnt_internal_data *data = node->internal_data;
+
+	spin_lock_yield(&data->lock);
+
+	if (data->msc_node != NULL) {
+		struct vfs_node *msc_node = data->msc_node;
+		int r = msc_node->n_stat(msc_node, stat);
+
+		if (r == DE_MEDIA_CHANGED)
+			data->media_changed = 1;
+
+		return spin_unlock(&data->lock), r;
+	}
+
+	return spin_unlock(&data->lock), 0;
+}
+
+static struct vfs_node *alloc_node(void)
+{
+	struct vfs_node *node;
+	const size_t F = 0x0F;
+	size_t size = sizeof(*node);
+	size_t data_offset;
+
+	size = (size + F) & (~F);
+	data_offset = size;
+
+	size += sizeof(struct usb_mnt_internal_data);
+
+	if ((node = malloc(size)) != NULL) {
+		addr_t a = (addr_t)node + (addr_t)data_offset;
+
+		vfs_init_node(node, size);
+
+		node->count = 1;
+		node->type = vfs_type_block;
+		node->internal_data = (void *)a;
+
+		node->n_read = n_read;
+		node->n_write = n_write;
+		node->n_sync = n_sync;
+		node->n_stat = n_stat;
+	}
+
+	return node;
+}
+
 void usb_mnt_update(void)
 {
+	struct usb_mnt_internal_data *data;
+	struct vfs_node *msc_node;
+	int i;
 
+	spin_lock_yield(&usb_mnt_lock);
+
+	for (i = 0; i < 6; i++) {
+		if (usb_mnt_nodes[i] == NULL)
+			continue;
+
+		data = usb_mnt_nodes[i]->internal_data;
+		msc_node = data->msc_node;
+
+		spin_lock_yield(&data->lock);
+
+		if (data->media_changed && msc_node != NULL) {
+			msc_node->n_release(&msc_node);
+			data->msc_node = NULL;
+			data->media_changed = 0;
+		}
+
+		spin_unlock(&data->lock);
+	}
+
+	for (i = 0; i < 6; i++) {
+		char name[] = "/dev/dancy-usb-storage/?";
+
+		name[23] = (char)('0' + i);
+
+		if (vfs_open(&name[0], &msc_node, 0, 0))
+			continue;
+
+		if (usb_mnt_nodes[i] == NULL)
+			usb_mnt_nodes[i] = alloc_node();
+
+		if (usb_mnt_nodes[i] != NULL) {
+			data = usb_mnt_nodes[i]->internal_data;
+
+			spin_lock_yield(&data->lock);
+
+			if (data->msc_node == NULL) {
+				vfs_increment_count(msc_node);
+				data->msc_node = msc_node;
+			}
+
+			spin_unlock(&data->lock);
+
+			strcpy(&name[0], "/mnt/?");
+			name[5] = (char)('u' + i);
+
+			mount_drive(&name[0], usb_mnt_nodes[i]);
+		}
+
+		msc_node->n_release(&msc_node);
+	}
+
+	spin_unlock(&usb_mnt_lock);
 }
