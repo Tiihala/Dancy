db01868dba2ceb85d85cab3b3f7a1ebe44694965
Antti Tiihala <Tiihala@users.noreply.github.com>
2025-05-25 11:26:16 +0300

    Fix programs/reboot
    A long long arg requires a proper syscall function.

--- arctic/programs/reboot/operate.c
+++ arctic/programs/reboot/operate.c
@@ -33,7 +33,7 @@ int operate(struct options *opt)
 	if (opt->poweroff)
 		request = __DANCY_REQUEST_POWER_OFF;
 
-	r = (int)__dancy_syscall2(__dancy_syscall_reboot, request, arg);
+	r = (int)__dancy_syscall2e(__dancy_syscall_reboot, request, arg);
 
 	if (r < 0)
 		errno = -r;
