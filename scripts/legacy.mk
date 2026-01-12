7e4270cbef58af5b4526e267eadd6e9d8b56c4a6
Antti Tiihala <Tiihala@users.noreply.github.com>
2026-01-12 19:18:54 +0200

    Fix legacy.mk
    In a distclean repository, calling 'make ./system/IN_IA16.AT' failed.
    An assembler needs to be built first, and now this works properly.

--- scripts/legacy.mk
+++ scripts/legacy.mk
@@ -6,6 +6,6 @@ DANCY_LEGACY_ASM= \
  ./boot/in_ia16/in_ia16.asm \
  ./boot/in_ia16/inc/int20h.asm \
 
-./system/IN_IA16.AT: $(DANCY_LEGACY_ASM) ./bin/dy-init$(DANCY_EXE)
+./system/IN_IA16.AT: $(DANCY_DEPS) $(DANCY_LEGACY_ASM)
 	$(DANCY_AS) -I./boot/in_ia16/ -fbin -o$@ ./boot/in_ia16/in_ia16.asm
 	$(DY_INIT) -tia16 --set-header $@
