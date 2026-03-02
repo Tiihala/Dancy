1f3c680661405385c56bab877a78ee6d74581316
Antti Tiihala <Tiihala@users.noreply.github.com>
2026-03-02 18:40:14 +0200

    Develop inttypes.h
    Add some printf() macros for signed and unsigned integers.

--- arctic/include/inttypes.h
+++ arctic/include/inttypes.h
@@ -25,6 +25,42 @@
 
 __Dancy_Header_Begin
 
+/*
+ * The printf() macros for signed integers.
+ */
+#define PRId8  "d"
+#define PRId16 "d"
+#define PRId32 "d"
+#define PRId64 "lld"
+
+#define PRIi8  "i"
+#define PRIi16 "i"
+#define PRIi32 "i"
+#define PRIi64 "lli"
+
+/*
+ * The printf() macros for unsigned integers.
+ */
+#define PRIo8  "o"
+#define PRIo16 "o"
+#define PRIo32 "o"
+#define PRIo64 "llo"
+
+#define PRIu8  "u"
+#define PRIu16 "u"
+#define PRIu32 "u"
+#define PRIu64 "llu"
+
+#define PRIx8  "x"
+#define PRIx16 "x"
+#define PRIx32 "x"
+#define PRIx64 "llx"
+
+#define PRIX8  "X"
+#define PRIX16 "X"
+#define PRIX32 "X"
+#define PRIX64 "llX"
+
 __Dancy_Header_End
 
 #endif
